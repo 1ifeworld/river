bda31b37342b7f6bc0686da08ee6d341807247dc
Salief <salieflewis@gmail.com>
2023-08-25 13:45:45 -0400

    add auth placeholder

---
+++ apps/frontend/components/AuthPlaceholder.tsx
@@ -0,0 +1,13 @@
+export function AuthPlaceholder() {
+  return (
+    <svg
+      width='29'
+      height='29'
+      viewBox='0 0 29 29'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <circle cx='14.8516' cy='14.272' r='14' fill='#E8E8E8' />
+    </svg>
+  );
+}
--- apps/frontend/components/client/Header.tsx
+++ apps/frontend/components/client/Header.tsx
@@ -1,17 +1,18 @@
-'use client'
+'use client';
 
 import { RiverIcon } from '@river/design-system';
+import { AuthPlaceholder } from '../AuthPlaceholder';
 
 export function Header() {
   return (
-    <div className='flex justify-between items-center p-4'>
+    <div className='flex justify-between items-center p-3'>
       {/* sidebar icon placeholder */}
       <div></div>
 
       <RiverIcon />
 
       {/* auth component placeholder */}
-      <div></div>
+      <AuthPlaceholder />
     </div>
   );
 }
