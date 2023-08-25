1f563c75b7e0e7bc3cafef1f8189819f9bdc21da
Salief <salieflewis@gmail.com>
2023-08-25 11:33:48 -0400

    add providers component

---
+++ apps/frontend/app/providers.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { ConnectKitProvider } from 'connectkit'
+import * as React from 'react'
+import { WagmiConfig } from 'wagmi'
+import { config } from '../wagmi'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => setMounted(true), [])
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider theme='minimal'>
+        {mounted && children}
+      </ConnectKitProvider>
+    </WagmiConfig>
+  )
+}
