4e5fbaa24417381d06870ed24fcbe2923f1f0ced
Salief <salieflewis@gmail.com>
2023-08-25 13:23:05 -0400

    add header component

---
+++ apps/frontend/components/client/Header.tsx
@@ -0,0 +1,15 @@
+import { RiverIcon } from '@river/design-system';
+
+export function Header() {
+  return (
+    <div className='flex justify-between items-center p-4'>
+      {/* sidebar icon placeholder */}
+      <div></div>
+
+      <RiverIcon />
+
+      {/* auth component placeholder */}
+      <div></div>
+    </div>
+  );
+}
