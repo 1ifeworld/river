0b6f2e4e6393e73c1e2dab69bcb0cfc06a446e1b
Salief <salieflewis@gmail.com>
2023-08-25 11:32:13 -0400

    swap next config

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      config.externals.push("pino-pretty", "lokijs", "encoding");
+      return config;
+    },
+  };
