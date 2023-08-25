756f1a97b2a81173be7c7dfebf3040ef8e935701
jawndiego <jawndiego@gmail.com>
2023-08-24 22:47:30 -0400

    this commit provides a base schema that ponder.config.ts references to generate a proper schema.grapql and an index.ts inside a generated folder. in Router.ts i provided a basic create command so we can write PressDataUpdated to the SQLite table . we can switch to postgres later. i need to figure out how to make it so we can make composite ids from two fields like we did in prisma for the update function specifically or else we will encounter unique id constraints errors

--- apps/backend/src/Router.ts
+++ apps/backend/src/Router.ts
@@ -7,3 +7,16 @@ ponder.on("Router:FactoryRegistered", async ({ event, context }) => {
 ponder.on("Router:OwnershipTransferred", async ({ event, context }) => {
   console.log(event.params);
 });
+
+ponder.on("Router:PressDataUpdated", async ({event, context}) => {
+const { TokenStorage } = context.entities;
+
+const pressDataUpdated = await TokenStorage.create({
+  id: event.params.sender,
+  data:{
+    sender: event.params.sender,
+    press: event.params.press,
+    pointer: event.params.pointer,
+  },
+})
+});
