68da353f5c95f531dc1033a2634fc7228c0031a2
Salief <salieflewis@gmail.com>
2023-08-28 10:16:49 -0400

    add helvetica neue, add colocated fonts dir, add empty public dir, resolves #46

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,12 +1,6 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local'
-
-// Font files can be colocated inside of `app`
-const helveticaNeue = localFont({
-  src: './my-font.woff2',
-  display: 'swap',
-})
+import { helveticaNeue } from './fonts/fonts';
 
 export const metadata: Metadata = {
   title: 'River',
@@ -19,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={`${helveticaNeue.variable}`}>
       <body>{children}</body>
     </html>
   );
--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-helvetica-neue)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
