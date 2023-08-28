4fb28d28d744f636a13275251153f89dce4790b5
Salief <salieflewis@gmail.com>
2023-08-28 10:06:21 -0400

    swap otf for woff2 files

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,5 +1,12 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
+import localFont from 'next/font/local'
+
+// Font files can be colocated inside of `app`
+const helveticaNeue = localFont({
+  src: './my-font.woff2',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'River',
