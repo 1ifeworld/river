2dd8048aa87f2329c2b33133f925dba88211aecc
0xTranqui <thinkandchill11@gmail.com>
2023-08-29 18:43:05 -0400

    refining everything

--- apps/frontend/components/client/AddToChannelModal/states/search/SearchInput.tsx
+++ apps/frontend/components/client/AddToChannelModal/states/search/SearchInput.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-// SearchInput.tsx
 import React, { useState, useEffect } from "react";
 import {
   Select,
@@ -9,6 +8,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@river/design-system/src/components/Select";
+import { SearchIcon } from "@river/design-system";
+import { Input } from "@river/design-system/src/components/Input";
+import { Label } from "@river/design-system/src/components/Label";
+
 import { networks } from "../../../../../constants";
 import useGetTokenMetadata from "../../../../../hooks/useGetTokenMetadata";
 import { Nft } from "alchemy-sdk";
@@ -30,25 +33,28 @@ function NetworkSelect({
 }: NetworkSelectProps) {
   return (
     <Select onValueChange={(value) => setSelectedNetwork(Number(value))}>
-      <SelectTrigger className="w-fit h-full">
+      <SelectTrigger
+        id="networkSelect"
+        className="w-[70px] h-[40px] px-2 border-[1.5px] border-[#DADADA] focus:outline-none"
+      >
         <SelectValue>
           <img
             src={getIconPath(selectedNetwork)}
+            width={14}
+            height={24}
             alt={networks[selectedNetwork] + " icon"}
-            className="w-5 h-5 mr-2"
+            className=""
           />
         </SelectValue>
       </SelectTrigger>
       <SelectContent>
         {Object.entries(networks).map(([networkId, networkName]) => (
           <SelectItem key={networkId} value={networkId}>
-            <div className="flex items-center">
-              {" "}
-              {/* Wrap in a flex container */}
+            <div className="flex items-center border-[1.5px] border-[#DADADA] px-2">
               <img
                 src={getIconPath(Number(networkId))}
                 alt={`${networkName} icon`}
-                className="w-5 h-5 mr-2"
+                className="w-[14px] h-[24px] mr-2"
               />
               {networkName}
             </div>
@@ -60,17 +66,20 @@ function NetworkSelect({
 }
 
 type SearchInputProps = {
-    searchResults: Nft | undefined;
-    setSearchResults: React.Dispatch<React.SetStateAction<Nft | undefined>>;
-    setSearchParams: (params: {
-        network?: number;
-        contractAddress?: string;
-        tokenId?: string;
-    }) => void;
+  searchResults: Nft | undefined;
+  setSearchResults: React.Dispatch<React.SetStateAction<Nft | undefined>>;
+  setSearchParams: (params: {
+    network?: number;
+    contractAddress?: string;
+    tokenId?: string;
+  }) => void;
 };
 
-
-const SearchInput = ({ searchResults, setSearchResults, setSearchParams }: SearchInputProps) => {
+const SearchInput = ({
+  searchResults,
+  setSearchResults,
+  setSearchParams,
+}: SearchInputProps) => {
   // Change the initial state and state type to number instead of string | null
   const [network, setNetwork] = useState<number>(1); // defaulting to Ethereum's ID
   const [contractAddress, setContractAddress] = useState<string>("");
@@ -82,14 +91,8 @@ const SearchInput = ({ searchResults, setSearchResults, setSearchParams }: Searc
     tokenId: tokenId,
   });
 
-  console.log("network: ", network);
-  console.log("contractAddress: ", contractAddress);
-  console.log("tokenId: ", tokenId);
-
-  console.log("search results in input compo: ", searchResults);
-
+  // trigger target metadata fetch
   const handleFetchMetadata = () => {
-    // trigger metadata fetch
     fetchMetadata();
   };
 
@@ -101,42 +104,66 @@ const SearchInput = ({ searchResults, setSearchResults, setSearchParams }: Searc
 
   useEffect(() => {
     setSearchParams({
-        network,
-        contractAddress,
-        tokenId
+      network,
+      contractAddress,
+      tokenId,
     });
-}, [network, contractAddress, tokenId]);  
+  }, [network, contractAddress, tokenId]);
 
   return (
-    <div className="flex items-center w-full my-2">
-      <div className="flex-1">{/* Placeholder for the dropdown */}</div>
-      <NetworkSelect
-        selectedNetwork={network}
-        setSelectedNetwork={setNetwork}
-      />
-      <input
-        type="text"
-        placeholder="0xA7b..."
-        className="flex-2 mx-2 p-1 border"
-        value={contractAddress}
-        onChange={(e) => setContractAddress(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="17"
-        className="flex-1 mx-2 p-1 border"
-        value={tokenId}
-        onChange={(e) => setTokenId(e.target.value)}
-      />
-      <button
-        className="flex-none w-12 mx-2 p-1 border"
-        onClick={() => {
-          // Handle the search here using network, contractAddress, and tokenId
-          handleFetchMetadata();
-        }}
-      >
-        Select
-      </button>
+    <div className="flex w-fit h-fit justify-start items-center border-[1px] border-red-500 ">
+      <div className="flex flex-col gap-y-[5px]">
+        <Label htmlFor="networkSelect" className="text-[#7B7B7B] w-fit ml-2 text-[10px]" id="networkSelect">
+          Network
+        </Label>
+        <NetworkSelect
+          selectedNetwork={network}
+          setSelectedNetwork={setNetwork}
+        />
+      </div>
+      <div className="flex flex-col gap-y-[5px]">
+        <Label htmlFor="addressInput" className="text-[#7B7B7B] ml-2 w-fit text-[10px]">
+          Address
+        </Label>
+        <Input
+          id="addressInput"
+          type="text"
+          placeholder="0xA7b..."
+          value={contractAddress}
+          onChange={(e) => setTokenId(e.target.value)}
+          className="px-2 w-[274px] h-[40px] border-[1.5px] border-l-0 border-[#DADADA] focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-y-[5px]">
+        <Label htmlFor="tokenIdInput" className="ml-2 text-[#7B7B7B] w-fit text-[10px]">
+          ID
+        </Label>
+        <Input
+          id="tokenIdInput"
+          type="text"
+          placeholder="17"
+          value={tokenId}
+          onChange={(e) => setTokenId(e.target.value)}
+          className="px-2 w-[40px] h-[40px] border-[1.5px] border-l-0 border-[#DADADA] focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-y-[5px] justify-center">
+        <Label htmlFor="" className="text-white w-fit text-[10px]">
+          Search
+        </Label>
+        <button
+          className="ml-[12px] flex h-full self-center rounded-full bg-[#F2F2F2] hover:bg-[#A8A8A8] disabled:bg-[#F2F2F2]"
+          onClick={() => {
+            // Handle the search here using network, contractAddress, and tokenId
+            handleFetchMetadata();
+          }}
+        >
+          <SearchIcon
+            width="35"
+            className="fill-[#393939] hover:fill-[#FEFEFE] disabled:fill-[#CACACA]"
+          />
+        </button>
+      </div>
     </div>
   );
 };
---
+++ packages/design-system/src/icons/SearchIcon.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { cn } from "../utils";
+
+export interface SvgProps
+  extends Pick<React.SVGProps<SVGSVGElement>, "fill" | "width" | "className"> {}
+
+export function SearchIcon({
+  width,
+  fill,
+  className,
+}: SvgProps) {
+  return (
+    <svg
+      width={width}
+      height={width}
+      viewBox="0 0 35 35"
+      // fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn(className)}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M19.481 19.4811C19.761 19.2011 20.2149 19.2011 20.4948 19.4811L24.29 23.2763C24.57 23.5562 24.57 24.0101 24.29 24.2901C24.0101 24.57 23.5562 24.57 23.2762 24.2901L19.481 20.4949C19.2011 20.2149 19.2011 19.761 19.481 19.4811Z"
+        // fill={fill}
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M16.1928 20.4518C18.545 20.4518 20.4518 18.545 20.4518 16.1928C20.4518 13.8406 18.545 11.9337 16.1928 11.9337C13.8406 11.9337 11.9337 13.8406 11.9337 16.1928C11.9337 18.545 13.8406 20.4518 16.1928 20.4518ZM16.1928 21.8856C19.3368 21.8856 21.8856 19.3368 21.8856 16.1928C21.8856 13.0487 19.3368 10.5 16.1928 10.5C13.0487 10.5 10.5 13.0487 10.5 16.1928C10.5 19.3368 13.0487 21.8856 16.1928 21.8856Z"
+        // fill={fill}
+      />
+    </svg>
+  );
+}
