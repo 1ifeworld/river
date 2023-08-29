ad22633722896b227c25f358e683f484e353f7f9
Salief <salieflewis@gmail.com>
2023-08-29 13:01:59 -0400

    push up to test dynamic segment on vercel

--- apps/frontend/app/channel/[contract]/page.tsx
+++ apps/frontend/app/channel/[contract]/page.tsx
@@ -1,5 +1,32 @@
-import { getListings } from '../gql/requests/getListings';
+import { getListings } from '../../../gql/requests/getListings';
+import { ChannelCard } from '../../../components/client';
+import { type Hex } from 'viem';
 
-const { channels } = await getListings({
-    channel: '0x5A2AfcD3aA9B1445A49f0bc8f9c11bFe3DA391de',
+// http://localhost:3000/channel/0x5A2AfcD3aA9B1445A49f0bc8f9c11bFe3DA391de
+
+export default async function Channel({
+  params,
+}: {
+  params: { channel: Hex };
+}) {
+  const { channels: listings } = await getListings({
+    channel: params.channel,
   });
+
+  return (
+    <>
+      <p>{params.channel}</p>
+
+      <div className={`grid grid-cols-6 gap-x-5 gap-y-4`}>
+        {/* {listings.map((listing, index) => (
+        <ChannelCard
+          key={`${listing}-${index}`}
+          // @ts-ignore
+          channel={listing}
+          width={224}
+        />
+      ))} */}
+      </div>
+    </>
+  );
+}
--- apps/frontend/gql/requests/getListings.ts
+++ apps/frontend/gql/requests/getListings.ts
@@ -2,7 +2,7 @@ import sdk from '../client';
 import { type Hex } from 'viem';
 
 export async function getListings({ channel }: { channel: Hex }) {
-  const { channels } = await sdk.listings({ channel });
+  const { channels } = await sdk.listings({ channel: channel.toString() });
 
   return {
     channels,
