b411023d2b8ae3b4b6ac2d9364aefd2eacccee6c
Salief <salieflewis@gmail.com>
2023-08-28 21:06:13 -0400

    remove river icon from channel view

--- apps/frontend/components/client/Header.tsx
+++ apps/frontend/components/client/Header.tsx
@@ -3,11 +3,10 @@ import { Connect } from './Connect';
 
 export function Header() {
   return (
-    <div className='flex justify-between items-center p-3'>
+    <div className='flex justify-end items-center p-3'>
       {/* sidebar icon placeholder */}
       <div></div>
 
-      <RiverIcon />
       <Connect />
     </div>
   );
