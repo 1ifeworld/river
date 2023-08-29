5277516154cb3286eeec5338e7d07793bc5868bf
Salief <salieflewis@gmail.com>
2023-08-29 13:37:27 -0400

    configure routing for channels, resolves #58

--- apps/frontend/app/channel/[contract]/page.tsx
+++ apps/frontend/app/channel/[contract]/page.tsx
@@ -1,32 +1,14 @@
 import { getListings } from '../../../gql/requests/getListings';
-import { ChannelCard } from '../../../components/client';
 import { type Hex } from 'viem';
-
-// http://localhost:3000/channel/0x5A2AfcD3aA9B1445A49f0bc8f9c11bFe3DA391de
 
 export default async function Channel({
   params,
 }: {
-  params: { channel: Hex };
+  params: { contract: Hex };
 }) {
   const { channels: listings } = await getListings({
-    channel: params.channel,
+    channel: params.contract,
   });
 
-  return (
-    <>
-      <p>{params.channel}</p>
-
-      <div className={`grid grid-cols-6 gap-x-5 gap-y-4`}>
-        {/* {listings.map((listing, index) => (
-        <ChannelCard
-          key={`${listing}-${index}`}
-          // @ts-ignore
-          channel={listing}
-          width={224}
-        />
-      ))} */}
-      </div>
-    </>
-  );
+  return <></>;
 }
