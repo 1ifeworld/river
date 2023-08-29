3acfbb6c9fb4ee642f12d72294b5e092912d2d4c
Salief <salieflewis@gmail.com>
2023-08-29 12:05:06 -0400

    add test channel

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,14 +1,13 @@
-import { 
-  Header, 
-  ChannelCard 
-} from '../components/client';
+import { Header, ChannelCard } from '../components/client';
 import { type Channel } from '../components/client';
+import { getListings } from '../gql/requests/getListings';
 
 const sampleChannelData: Channel = {
-  name: "Channel",
-  creator: "tranqui.eth",
-  cover: "https://ipfs.io/ipfs/bafybeihax3e3suai6qrnjrgletfaqfzriziokl7zozrq3nh42df7u74jyu"
-}
+  name: 'Channel',
+  creator: 'tranqui.eth',
+  cover:
+    'https://ipfs.io/ipfs/bafybeihax3e3suai6qrnjrgletfaqfzriziokl7zozrq3nh42df7u74jyu',
+};
 
 const arrayOfChannelData: Channel[] = [
   sampleChannelData,
@@ -18,14 +17,17 @@ const arrayOfChannelData: Channel[] = [
   sampleChannelData,
   sampleChannelData,
   sampleChannelData,
-  sampleChannelData  
-]
+  sampleChannelData,
+];
 
 export default async function Home() {
+  const channels = await getListings({
+    channel: '0x5A2AfcD3aA9B1445A49f0bc8f9c11bFe3DA391de',
+  });
   return (
     <>
       <Header />
-      <main className="mx-[144px] my-5">
+      <main className='mx-[144px] my-5'>
         <div className={`grid grid-cols-6 gap-x-5 gap-y-4`}>
           {arrayOfChannelData.map((channel, index) => (
             <ChannelCard
@@ -34,7 +36,7 @@ export default async function Home() {
               width={224}
             />
           ))}
-        </div>        
+        </div>
       </main>
     </>
   );
