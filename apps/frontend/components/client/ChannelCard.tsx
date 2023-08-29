600d60c2dcdbce28be528822dcfd060cb6e8c075
Salief <salieflewis@gmail.com>
2023-08-29 10:12:44 -0400

    strip context menu

--- apps/frontend/components/client/ChannelCard.tsx
+++ apps/frontend/components/client/ChannelCard.tsx
@@ -1,15 +1,5 @@
 import Image from "next/image";
 
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuSeparator,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
-} from "@river/design-system/src/components/ContextMenu";
 import { cn } from "@river/design-system/src/utils";
 
 export interface Channel {
@@ -38,8 +28,7 @@ export function ChannelCard({
 }: ChannelCardProps) {
   return (
     <div className={cn("space-y-[8px]", className)} {...props}>
-      <ContextMenu>
-        <ContextMenuTrigger>
+  
           <div className="overflow-hidden rounded-[4px]">
             <Image
               src={channel.cover}
@@ -51,45 +40,7 @@ export function ChannelCard({
               )}
             />
           </div>
-        </ContextMenuTrigger>
-        {/* <ContextMenuContent className="w-40">
-            <ContextMenuItem>Add to Library</ContextMenuItem>
-            <ContextMenuSub>
-              <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
-              <ContextMenuSubContent className="w-48">
-                <ContextMenuItem>
-                  <PlusCircledIcon className="mr-2 h-4 w-4" />
-                  New Playlist
-                </ContextMenuItem>
-                <ContextMenuSeparator />
-                {playlists.map((playlist) => (
-                  <ContextMenuItem key={playlist}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="mr-2 h-4 w-4"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 15V6M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM12 12H3M16 6H3M12 18H3" />
-                    </svg>
-                    {playlist}
-                  </ContextMenuItem>
-                ))}
-              </ContextMenuSubContent>
-            </ContextMenuSub>
-            <ContextMenuSeparator />
-            <ContextMenuItem>Play Next</ContextMenuItem>
-            <ContextMenuItem>Play Later</ContextMenuItem>
-            <ContextMenuItem>Create Station</ContextMenuItem>
-            <ContextMenuSeparator />
-            <ContextMenuItem>Like</ContextMenuItem>
-            <ContextMenuItem>Share</ContextMenuItem>
-          </ContextMenuContent> */}
-      </ContextMenu>
+       
       <div className="space-y-1 text-sm">
         <h3 className="font-medium leading-none">{channel.name}</h3>
         <p className="text-xs text-muted-foreground">{channel.creator}</p>
