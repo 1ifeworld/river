7089efe3a95da7750e6868955746856e5b7cc691
Salief <salieflewis@gmail.com>
2023-08-30 10:29:43 -0400

    change tw config to js file

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -28,4 +27,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
