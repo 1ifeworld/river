e445f0f828b4a2671934f3a76226a15a8c884640
Salief <salieflewis@gmail.com>
2023-08-30 11:11:39 -0400

    add tailwind animate

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@river/tailwind";
 
 export default {
-  content: [...baseConfig.content, "../../packages/ui/src/**/*.{ts,tsx}"],
+  content: [...baseConfig.content, "../../packages/design-system/src/**/*.{ts,tsx}"],
   presets: [baseConfig],
 } satisfies Config;
