a1f8e6405f58c93ee3983b262b10906959949a53
0xTranqui <thinkandchill11@gmail.com>
2023-08-30 18:32:30 -0400

    move header/connect into layout to save on redundant usage

--- apps/frontend/app/channel/[contract]/page.tsx
+++ apps/frontend/app/channel/[contract]/page.tsx
@@ -1,13 +1,18 @@
 import { getListings } from '../../../gql/requests/getListings';
+import { Flex } from '@river/design-system';
 
 export default async function Channel({
   params,
 }: {
   params: { contract: string };
 }) {
-  const { channels: listings } = await getListings({
-    channel: params.contract,
-  });
+  // const { channels: listings } = await getListings({
+  //   channel: params.contract,
+  // });
 
-  return <></>;
+  return (
+    <Flex>
+
+    </Flex>
+  );
 }
