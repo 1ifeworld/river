c8670fa4cceac48d08b4b016f42e3749773a621f
Salief <salieflewis@gmail.com>
2023-09-06 13:09:01 -0400

    update the channel card component, resolves #130

--- apps/frontend/components/client/ChannelCard.tsx
+++ apps/frontend/components/client/ChannelCard.tsx
@@ -1,29 +1,35 @@
 import Image from 'next/image';
 import { Body, Card, BodySmall, Stack, cn } from '@river/design-system';
 import { type Channel } from '../../types/types';
+import { truncateText } from '../../utils';
 
 export function ChannelCard({
   channel,
   className,
 }: {
   channel: Channel;
-  className: string;
+  className?: string;
 }) {
   return (
     <Stack className={cn('gap-y-2', className)}>
       {/* Image */}
       <Card>
-        <Image src={channel.cover} alt={channel.name} />
+        <Image
+          src={channel.cover}
+          alt={channel.name}
+          width={224}
+          height={224}
+        />
       </Card>
       {/* Caption */}
-      <Stack>
-        <Body className='text-label font-medium'>{channel.name}</Body>
+      <Stack className='max-w-[224px]'>
+        <Body className='text-label font-medium leading-none'>{channel.name}</Body>
         {channel.members ? (
           <BodySmall className='text-labelMuted'>
-            {channel.creator} + {channel.members.length} others
+            {truncateText(channel.creator, 30)} + {channel.members.length} others
           </BodySmall>
         ) : (
-          <BodySmall className='text-labelMuted'>{channel.creator}</BodySmall>
+          <BodySmall className='text-labelMuted'>{truncateText(channel.creator, 30)}</BodySmall>
         )}
       </Stack>
     </Stack>
