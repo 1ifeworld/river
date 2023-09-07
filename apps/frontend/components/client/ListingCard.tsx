949a7e1721411c4d3587b4c1472745022f51d730
Salief <salieflewis@gmail.com>
2023-09-07 14:08:04 -0400

    add listing card component, resolves #148

---
+++ apps/frontend/components/client/ListingCard.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Body, Card, BodySmall, Stack, cn } from '@river/design-system';
+import { type ListingExtended } from '../../types/types';
+import { truncateText } from '../../utils';
+
+export function ListingCard({
+  extendedListing,
+  className,
+}: {
+  extendedListing: ListingExtended;
+  className?: string;
+}) {
+  return (
+    <Stack className={cn('gap-y-2', className)}>
+      {/* Image */}
+      <Card>
+        <Link href={extendedListing.listingAddress}>
+          <Image
+            src={
+              extendedListing?.listingTargetMetadata?.pieceImageURL as string
+            }
+            alt={extendedListing?.listingTargetMetadata?.pieceName as string}
+            width={224}
+            height={224}
+          />
+        </Link>
+      </Card>
+      {/* Caption */}
+      <Stack className='max-w-[224px]'>
+        <Body className='text-label font-medium leading-none'>
+          {extendedListing?.listingTargetMetadata?.pieceName}
+        </Body>
+        {extendedListing?.channel?.members ? (
+          <BodySmall className='text-label-muted'>
+            {truncateText(extendedListing?.createdBy, 30)} +{' '}
+            {extendedListing?.channel?.members.length} others
+          </BodySmall>
+        ) : (
+          <BodySmall className='text-label-muted'>
+            {truncateText(extendedListing?.createdBy, 30)}
+          </BodySmall>
+        )}
+      </Stack>
+    </Stack>
+  );
+}
