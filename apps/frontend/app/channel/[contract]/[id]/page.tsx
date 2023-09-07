a34128b7021da1de16e99cad58764f1db5da9a2d
0xTranqui <thinkandchill11@gmail.com>
2023-09-07 00:53:29 -0400

    implemented query in server component and added type safety checks to the route so that anything can get passed in after channel/ and the site wont break

--- apps/frontend/app/channel/[contract]/[id]/page.tsx
+++ apps/frontend/app/channel/[contract]/[id]/page.tsx
@@ -1,30 +1,84 @@
-import { getListing } from '../../../../gql/requests/getListing';
-import { Hex } from 'viem';
-import { Flex } from '@river/design-system';
+import { getListing } from "../../../../gql/requests/getListing";
+import { Hex, isAddress } from "viem";
+import { Flex } from "@river/design-system";
+import { getAddress } from "viem";
 
+// Define a type for a numeric string (e.g., "123")
+type NumericString = `${number}`;
+
+// Define a type for a listing ID which follows a specific pattern
+type ListingId = `${NumericString}/${Hex}/${string}`;
+
+// Utility function to check if a given string is a valid hexadecimal value
+function isValidHex(value: string): value is Hex {
+  return /^0x[a-fA-F0-9]+$/.test(value);
+}
+
+// Utility function to check if a given string is a valid numeric string
+function isValidNumericString(value: string): value is NumericString {
+  return /^\d+$/.test(value);
+}
+
+// Main server component function
 export default async function View({
   params,
 }: {
-  params: { contract: string, id: string };
+  params: { contract: Hex; id: string };
 }) {
+  // Initialize state variables
+  let success = true; // Flag to track if the input validation and query are successful
+  let error: string | null = null; // To store error messages, if any
+  let queryResult: any; // To store the result of the query
 
-  console.log("what is the id: ", params.id)
-  console.log("what is the params: ", params.contract)
-  console.log("what is the full listing: ", "1/"+params.contract+"/"+params.id)
+  // Validate if the contract parameter is a valid Ethereum address
+  if (!isAddress(params.contract)) {
+    success = false;
+    error = "Contract input was not an address";
+  }
 
+  // Validate if the ID parameter is a valid numeric string
+  if (success && !isValidNumericString(params.id)) {
+    success = false;
+    error = "Invalid ID provided.";
+  }
 
+  // If all validations pass, construct the listing ID and fetch the listing data
+  if (success) {
+    const constructListingId: ListingId = `${1}/${
+      getAddress(params.contract) as Hex
+    }/${params.id}`;
 
-  // const { listings } = await getListing({
-  //   id: params.id
-  // });
+    // Fetch the listing data using the constructed ID
+    const { listings } = await getListing({
+      id: constructListingId as string,
+    });
 
+    queryResult = listings; // Store the fetched data in the queryResult variable
+  }
 
-  return (
+  // Render different components or elements based on the state variables
 
-    <Flex className='flex-col '>
+  // If there's an error, render the error message
+  if (!success && error) {
+    return <Flex className="flex-col">{error}</Flex>;
+  }
 
-      yo
-    </Flex>
+  // If the query result is an empty array, render a message indicating no listings found
+  if (queryResult && queryResult.length === 0) {
+    return (
+      <Flex className="flex-col">
+        No listings found for the provided listingId
+      </Flex>
+    );
+  }
 
-  );
+  // If the query result has data, render the data
+  if (queryResult && queryResult.length !== 0) {
+    return (
+      <Flex className="flex-col">Data: {JSON.stringify(queryResult)}</Flex>
+    );
+  }
+
+  // Default render (e.g., while waiting for data or initial state)
+  return <Flex className="flex-col">Loading...</Flex>;
 }
