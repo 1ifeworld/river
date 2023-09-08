eedb76f5391b0a10405b78992f035726afbabd64
jawndiego <jawndiego@gmail.com>
2023-09-07 20:36:29 -0400

    exported app/page with All channels component commented out for the sake of the build

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from '../components/client';
 import { AllChannels } from '../components/channel/AllChannels';
-import { type Channel } from '../types/types';
 import { getAllChannels } from '../gql/requests/getAllChannels';
 import { Stack, Flex } from '@river/design-system';
 import { Hex } from 'viem';
@@ -8,32 +7,32 @@ import { Hex } from 'viem';
 export default async function Home() {
   const { channels } = await getAllChannels();
 
-  const ipfsToHttps = (ipfsString: string) => {
-    if (!ipfsString) return '';
-    return ipfsString.replace('ipfs://', 'https://ipfs.io/ipfs/');
-  };
+  // const ipfsToHttps = (ipfsString: string) => {
+  //   if (!ipfsString) return '';
+  //   return ipfsString.replace('ipfs://', 'https://ipfs.io/ipfs/');
+  // };
 
-  // TODO JDR: make it work with uri
+  // TODO JDR: make it work with uri use in QUERY 
   const channelsWithNoName = channels.filter(
     (channel) =>
       channel?.contractUri?.image && channel.contractUri.image.trim() !== ''
   );
 
-  const channelsInput: Channel[] = channelsWithNoName.map((channel) => ({
-    id: channel?.id,
-    name: channel?.contractUri?.name as string,
-    description: channel?.contractUri?.description as string,
-    cover: ipfsToHttps(channel?.contractUri?.image as string),
-    creator: channel?.createdBy as Hex,
-    members: channel?.logicTransmitterMerkleAdmin[0]?.accounts as string[],
-  }));
+  // const channelsContractUriInput: ContractUri[] = channelsWithNoName.map((channel) => ({
+  //   id: channel?.id,
+  //   name: channel?.contractUri?.name as string,
+  //   description: channel?.contractUri?.description as string,
+  //   cover: ipfsToHttps(channel?.contractUri?.image as string),
+  //   creator: channel?.createdBy as Hex,
+  //   members: channel?.logicTransmitterMerkleAdmin[0]?.accounts as string[],
+  // }));
 
   return (
     <>
       <Stack className='items-center my-10'>
         {/* Channel card grid */}
         <Flex className='flex-wrap gap-5 pb-4'>
-          <AllChannels channel={channelsInput} />
+          {/* <AllChannels channel={channelsContractUriInput} /> */}
         </Flex>
       </Stack>
     </>
