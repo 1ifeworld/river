fe39ad567f281569ea64c589ebac1a41d4154455
jawndiego <jawndiego@gmail.com>
2023-09-12 00:25:40 -0400

    working createLanyard tree based on front end inputs. and testing get tree and get merkley functions

--- apps/frontend/app/channel/page.tsx
+++ apps/frontend/app/channel/page.tsx
@@ -2,8 +2,13 @@
 import React, { useState } from "react";
 import { useWeb3Storage } from "../../hooks/useWeb3Storage";
 import { Input, Button, Stack } from "@river/design-system";
+import { createLanyardTree } from "../../hooks";
 
-export default function ChannelPictureSetter() {
+function isValidAddress(address: string): address is `0x${string}` {
+  return address.startsWith('0x');
+}
+
+export default function  ChannelPictureSetter() {
     const [image, setImage] = useState<File | null>(null);
     const [cid, setCid] = useState<string>("");
     const [name, setName] = useState<string>("");
@@ -24,6 +29,7 @@ export default function ChannelPictureSetter() {
             setCid(imageCid);
         }
     };
+
     const handleContractInsertion = async () => {
         const contractUriData = {
           name: name,
@@ -41,8 +47,31 @@ export default function ChannelPictureSetter() {
         console.log(`Schema CID: ${schemaCid}`);
       
         // schema cid is now able to be sent to contract 
-      };
-      
+    };
+
+    const [addresses, setAddresses] = useState<string[]>([]);
+    const [inputAddress, setInputAddress] = useState<string>('');
+
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+      if (addresses.every(isValidAddress)) {
+        const response = await createLanyardTree(addresses as `0x${string}`[]);
+        console.log(response); // Display the result in the console
+        setAddresses([]);
+      } else {
+        console.error("One or more addresses are not valid.");
+      }
+    };
+    
+    const handleAddAddress = () => {
+      if (inputAddress) {
+        const newAddresses = inputAddress.split(',').map(addr => addr.trim());
+        setAddresses(prev => [...prev, ...newAddresses]);
+        setInputAddress('');
+      }
+    };
+
     return (
       <Stack>
         <Input 
@@ -57,8 +86,16 @@ export default function ChannelPictureSetter() {
           onChange={(e) => setDescription(e.target.value)} 
         />
         <Input type="file" onChange={handleImageChange} />
+        <Input 
+          type="text" 
+          placeholder="Enter addresses separated by commas"
+          value={inputAddress}
+          onChange={(e) => setInputAddress(e.target.value)}
+        />
+        <Button onClick={handleAddAddress}>Add Addresses</Button>
         <Button onClick={handleUpload}>Upload Image</Button>
         <Button onClick={handleContractInsertion}>Insert into Contract</Button>
+        <Button onClick={handleSubmit}>Create Lanyard Tree</Button>
         {cid && <img src={`https://ipfs.io/ipfs/${cid}`} alt="Profile" />}
       </Stack>
     );
---
+++ apps/frontend/hooks/useLanyardTree.ts
@@ -0,0 +1,69 @@
+import * as lanyard from "lanyard";
+import { Hash, Hex } from "viem";
+
+type OwnerAddress = `0x${string}`;
+
+interface LanyardTreeError {
+  message: string;
+}
+
+interface UseLanyardTreeResult {
+  merkle: Hash | null;
+  error: LanyardTreeError | null;
+}
+
+// need to adjust this to get data from form 
+export const createLanyardTree = async (
+  owners: OwnerAddress[]
+): Promise<UseLanyardTreeResult> => {
+  try {
+    if (!owners || owners.length === 0) {
+      throw new Error("No valid addresses provided");
+    }
+
+    const { merkleRoot } = await lanyard.createTree({
+      unhashedLeaves: owners,
+    });
+    console.log("merkleRoot", merkleRoot)
+    return { merkle: merkleRoot as Hash, error: null };
+  } catch (error: any) {
+    return { merkle: null, error: { message: error.message } };
+  }
+};
+
+export const getTreeFromRoot = async (
+  root: Hash | null, 
+  address: Hex, 
+): Promise<any> => {
+  if (!root) {
+    console.error("Root is null or undefined.");
+    return null;
+  }
+  try {
+    const basicTree = await lanyard.getTree(root);
+    console.log("root", root)
+    if (!basicTree) {
+      console.error("Failed to retrieve the basic tree.");
+      return null;
+    }
+    console.log('basic leaf count', basicTree.leafCount);
+    return basicTree;
+  } catch (error: any) { 
+    console.error("Error fetching the tree:", error.message);
+    return null;
+  }
+};
+
+export const getMerkleProofs = async (
+  merkleRoot: Hash,
+  address: Hex
+): Promise<any> => {
+  try {
+    const proof = await lanyard.getProof({ merkleRoot, address });
+    console.log(proof);
+    return proof;
+  } catch (error: any) {
+    console.error("Failed to get the Merkle proofs:", error.message);
+    return null;
+  }
+};
