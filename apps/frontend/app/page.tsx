4a7f5c9ddae7d3c28034ac43d8bc6b2a5250a649
0xTranqui <thinkandchill11@gmail.com>
2023-09-13 18:51:14 -0400

    add force dynamic to index route

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -3,6 +3,8 @@ import { Flex } from '@river/design-system';
 import { Channel } from '../gql/sdk.generated';
 import { ChannelCard } from '../components/client';
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const { channels } = await getAllChannels();
 
