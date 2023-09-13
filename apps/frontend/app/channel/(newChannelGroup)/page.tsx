d2dfe5a7928e08583925afaf617268a93bc4d635
0xTranqui <thinkandchill11@gmail.com>
2023-09-13 01:55:11 -0400

    build issues

--- apps/frontend/app/channel/(newChannelGroup)/page.tsx
+++ apps/frontend/app/channel/(newChannelGroup)/page.tsx
@@ -1,27 +1,13 @@
 "use client";
 
-import { LanyardMerkle } from "../../../components/client/ChannelUri/LanyardMerkle";
-import { Flex, Stack } from "@river/design-system";
-import { ChannelCreate } from "../../../components/client/newChannel/ChannelCreate";
+import { Stack } from "@river/design-system";
 import { NewChannelContainer } from "../../../components/client/newChannel/NewChannelContainer";
-import { useState, useEffect } from "react";
 
 export default function Page() {
-  const [cid, setCid] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
 
   return (
     <Stack>
       <NewChannelContainer />
-
-      {/* <div>
-
-
-      <LanyardMerkle />
-
-
-      </div> */}
     </Stack>
   );
 }
