3a523bfa49b90dfb32fcf72414ca743d7c6c0faa
Salief <salieflewis@gmail.com>
2023-09-13 09:43:49 -0400

    copy stylesheet over from river to estuary docs

--- apps/estuary-docs/app/estuary/components/input/page.tsx
+++ apps/estuary-docs/app/estuary/components/input/page.tsx
@@ -1,4 +1,4 @@
-import { Stack, BodyLarge, Headline } from '@river/design-system';
+import { Stack, BodyLarge, Body, Headline, Input } from '@river/design-system';
 
 export default function Page() {
   return (
@@ -6,6 +6,10 @@ export default function Page() {
       <Stack className='mb-8 gap-2'>
         <Headline>Input</Headline>
         <BodyLarge className='text-label-muted'>Area for user input</BodyLarge>
+        <Stack className='space-y-10'>
+        {/* Default */}
+        <Input />
+        </Stack>
       </Stack>
     </>
   );
