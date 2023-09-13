4c620184dff4c61f8f28e1959aa0c6909b644dfb
0xTranqui <thinkandchill11@gmail.com>
2023-09-13 18:58:18 -0400

    add logs

--- apps/frontend/components/client/newChannel/NewChannelContainer.tsx
+++ apps/frontend/components/client/newChannel/NewChannelContainer.tsx
@@ -1,21 +1,21 @@
-import { Flex, Stack } from '@river/design-system';
-import { useWeb3Storage } from '../../../hooks/useWeb3Storage';
-import { useState, useEffect } from 'react';
-import { ChannelUri } from '../ChannelUri/ChannelUri';
-import { UploadCard } from './UploadCard';
-import { CreateChannelButton } from '.';
-import { useSetupPress } from '../../../hooks';
+import { Flex, Stack } from "@river/design-system";
+import { useWeb3Storage } from "../../../hooks/useWeb3Storage";
+import { useState, useEffect } from "react";
+import { ChannelUri } from "../ChannelUri/ChannelUri";
+import { UploadCard } from "./UploadCard";
+import { CreateChannelButton } from ".";
+import { useSetupPress } from "../../../hooks";
 import {
   Hex,
   Hash,
   encodeAbiParameters,
   parseAbiParameters,
   zeroAddress,
-} from 'viem';
-import { factory, logic, renderer } from '../../../constants';
-import { useAccount } from 'wagmi';
-import { useRouter } from 'next/navigation';
-import { LanyardMerkle } from './LanyardMerkle';
+} from "viem";
+import { factory, logic, renderer } from "../../../constants";
+import { useAccount } from "wagmi";
+import { useRouter } from "next/navigation";
+import { LanyardMerkle } from "./LanyardMerkle";
 
 export function NewChannelContainer() {
   const { address } = useAccount();
@@ -23,12 +23,12 @@ export function NewChannelContainer() {
 
   const router = useRouter();
 
-  const [imageCid, setImageCid] = useState<string>('');
-  const [name, setName] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const [uriCid, setUriCid] = useState<string>('');
+  const [imageCid, setImageCid] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [uriCid, setUriCid] = useState<string>("");
   const [channelCreated, setChannelCreated] = useState(false);
-  const [merkleRoot, setMerkleRoot] = useState<string>('');
+  const [merkleRoot, setMerkleRoot] = useState<string>("");
 
   const { client } = useWeb3Storage(uriCid);
   const handleUriUpload = async () => {
@@ -38,9 +38,9 @@ export function NewChannelContainer() {
       image: `ipfs://${imageCid}`,
     };
     const blob = new Blob([JSON.stringify(contractUriData)], {
-      type: 'application/json',
+      type: "application/json",
     });
-    const file = new File([blob], 'schema.json', { type: 'application/json' });
+    const file = new File([blob], "schema.json", { type: "application/json" });
     const schemaCid = await client.put([file], { wrapWithDirectory: false });
     setUriCid(`ipfs://${schemaCid}`);
   };
@@ -49,21 +49,21 @@ export function NewChannelContainer() {
   // setup logic inits
   const initialAdminArray: Hex[] = [initialAdmin];
   const initialMerkleRoot: Hash =
-    '0x0000000000000000000000000000000000000000000000000000000000000000';
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
   const logicInit: Hash = encodeAbiParameters(
-    parseAbiParameters('address[], bytes32'),
+    parseAbiParameters("address[], bytes32"),
     [initialAdminArray, initialMerkleRoot]
   );
   // setup rendererInits
   const rendererInit: Hash =
-    '0x0000000000000000000000000000000000000000000000000000000000000000';
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
   // encode sub inputs
-  const encodedUri: Hash = encodeAbiParameters(parseAbiParameters('string'), [
+  const encodedUri: Hash = encodeAbiParameters(parseAbiParameters("string"), [
     uriCid,
   ]);
   const setupInputs: Hash = encodeAbiParameters(
     parseAbiParameters(
-      '(string, bytes, address, address, bytes, address, bytes)'
+      "(string, bytes, address, address, bytes, address, bytes)"
     ),
     [[name, encodedUri, initialAdmin, logic, logicInit, renderer, rendererInit]]
   );
@@ -72,6 +72,8 @@ export function NewChannelContainer() {
     data: setupInputs,
     prepareTxn: uriCid ? true : false,
   });
+
+  console.log('Txn receipt', setupPressTxnReceipt)
 
   const newChannelRoute = setupPressTxnReceipt
     ? setupPressTxnReceipt.logs[0].address
@@ -85,39 +87,40 @@ export function NewChannelContainer() {
     }
   }, [uriCid, setupPress]);
 
-// trigger route change once channel creation is complete
-useEffect(() => {
-  if (!!newChannelRoute) {
-    // Trigger revalidation for the home page
-    // fetch('/api/refresh', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ path: '/' }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //   if (data.revalidated) {
-    //     console.log('Home page revalidated successfully:', data);
-    //   } else {
-    //     console.error('Error triggering revalidation:', data.message);
-    //   }
-    // })
-    // .catch(error => {
-    //   console.error('Failed to trigger revalidation:', error);
-    // });
-    router.push(`/channel/${newChannelRoute}`);
-  }
-}, [newChannelRoute]);
+  // trigger route change once channel creation is complete
+  useEffect(() => {
+    if (!!newChannelRoute) {
+      console.log("New channel route:", newChannelRoute);
+      // Trigger revalidation for the home page
+      // fetch('/api/refresh', {
+      //   method: 'POST',
+      //   body: JSON.stringify({ path: '/' }),
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      // })
+      // .then(response => response.json())
+      // .then(data => {
+      //   if (data.revalidated) {
+      //     console.log('Home page revalidated successfully:', data);
+      //   } else {
+      //     console.error('Error triggering revalidation:', data.message);
+      //   }
+      // })
+      // .catch(error => {
+      //   console.error('Failed to trigger revalidation:', error);
+      // });
+      router.push(`/channel/${newChannelRoute}`);
+    }
+  }, [newChannelRoute]);
 
   return (
-    <Flex className='gap-x-10 h-[248px]'>
+    <Flex className="gap-x-10 h-[248px]">
       {/* First Column: Channel image upload */}
       <UploadCard imageCid={imageCid} setImageCid={setImageCid} />
       {/* Second Column: Channel name + description + create trigger */}
-      <Stack className='h-full justify-end cursor-default'>
-        <span className='inline-block mb-5'>
+      <Stack className="h-full justify-end cursor-default">
+        <span className="inline-block mb-5">
           <ChannelUri
             setName={setName}
             name={name}
