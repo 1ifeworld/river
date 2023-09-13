89f6b1f2a6e1ebc2adb6b6920b8d6bf8c82bb6a0
Salief <salieflewis@gmail.com>
2023-09-13 14:14:45 -0400

    edits to the upload card

--- apps/frontend/hooks/useSetupPress.ts
+++ apps/frontend/hooks/useSetupPress.ts
@@ -1,66 +1,64 @@
 import {
-    usePrepareContractWrite,
-    useContractWrite,
-    useWaitForTransaction,
-  } from 'wagmi';
-  import { PrepareWriteContractResult, WriteContractResult } from 'wagmi/actions';
-  import { type Hex, type Hash, type TransactionReceipt } from 'viem';
-  import { routerAbi } from '../abi';
-  import { router } from '../constants';
-  
-  interface SetupPressProps {
-    factory: Hex;
-    data: Hash;
-    prepareTxn: boolean;
-    successCallback?: () => void;
-  }
-  
-  interface SetupPressReturn {
-    setupPressConfig: PrepareWriteContractResult;
-    setupPress: (() => void) | undefined;
-    setupPressResult: WriteContractResult | undefined;
-    setupPressTxnReceipt: TransactionReceipt | undefined
-    setupPressLoading: boolean;
-    setupPressSuccess: boolean;
-  }
-  
-  export function useSetupPress({
-    factory,
-    data,
-    prepareTxn,
-    successCallback,
-  }: SetupPressProps): SetupPressReturn {
-    const { config: setupPressConfig } = usePrepareContractWrite({
-      address: router,
-      abi: routerAbi,
-      functionName: 'setupPress',
-      args: [factory, data],
-      value: BigInt(0), // harcoded to zero for setupPress call
-      enabled: prepareTxn,
-    });
-  
-    const { data: setupPressResult, write: setupPress } =
-      useContractWrite(setupPressConfig);
-  
-    const { data: setupPressTxnReceipt, isLoading: setupPressLoading, isSuccess: setupPressSuccess } =
-      useWaitForTransaction({
-        hash: setupPressResult?.hash,
-        onSuccess() {
-          console.log("txn complete data: ", setupPressTxnReceipt )
-          successCallback?.();
-          console.log("success callback ran")
-        },
-      });
-  
-      console.log("seuptPress txn receipt", setupPressTxnReceipt)
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from 'wagmi';
+import { PrepareWriteContractResult, WriteContractResult } from 'wagmi/actions';
+import { type Hex, type Hash, type TransactionReceipt } from 'viem';
+import { routerAbi } from '../abi';
+import { router } from '../constants';
 
-    return {
-      setupPressConfig,
-      setupPress,      
-      setupPressResult,
-      setupPressTxnReceipt,
-      setupPressLoading,
-      setupPressSuccess,
-    };
-  }
-  
+interface SetupPressProps {
+  factory: Hex;
+  data: Hash;
+  prepareTxn: boolean;
+  successCallback?: () => void;
+}
+
+interface SetupPressReturn {
+  setupPressConfig: PrepareWriteContractResult;
+  setupPress: (() => void) | undefined;
+  setupPressResult: WriteContractResult | undefined;
+  setupPressTxnReceipt: TransactionReceipt | undefined;
+  setupPressLoading: boolean;
+  setupPressSuccess: boolean;
+}
+
+export function useSetupPress({
+  factory,
+  data,
+  prepareTxn,
+  successCallback,
+}: SetupPressProps): SetupPressReturn {
+  const { config: setupPressConfig } = usePrepareContractWrite({
+    address: router,
+    abi: routerAbi,
+    functionName: 'setupPress',
+    args: [factory, data],
+    value: BigInt(0), // harcoded to zero for setupPress call
+    enabled: prepareTxn,
+  });
+
+  const { data: setupPressResult, write: setupPress } =
+    useContractWrite(setupPressConfig);
+
+  const {
+    data: setupPressTxnReceipt,
+    isLoading: setupPressLoading,
+    isSuccess: setupPressSuccess,
+  } = useWaitForTransaction({
+    hash: setupPressResult?.hash,
+    onSuccess() {
+      successCallback?.();
+    },
+  });
+
+  return {
+    setupPressConfig,
+    setupPress,
+    setupPressResult,
+    setupPressTxnReceipt,
+    setupPressLoading,
+    setupPressSuccess,
+  };
+}
