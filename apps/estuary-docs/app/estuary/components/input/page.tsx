ecb2935aeecea3294b4c136e1839903930a15bbc
Salief <salieflewis@gmail.com>
2023-09-13 09:48:42 -0400

    copy over fresh input from shadcn

--- apps/estuary-docs/app/estuary/components/input/page.tsx
+++ apps/estuary-docs/app/estuary/components/input/page.tsx
@@ -1,14 +1,25 @@
-import { Stack, BodyLarge, Body, Headline, Input } from '@river/design-system';
+import {
+  Stack,
+  BodyLarge,
+  Body,
+  BodySmall,
+  Headline,
+  Flex,
+  Input,
+} from '@river/design-system';
 
 export default function Page() {
   return (
     <>
       <Stack className='mb-8 gap-2'>
         <Headline>Input</Headline>
-        <BodyLarge className='text-label-muted'>Area for user input</BodyLarge>
-        <Stack className='space-y-10'>
-        {/* Default */}
-        <Input />
+        <BodyLarge className='text-label-muted mb-8'>Area for user input</BodyLarge>
+        <Stack className='gap-10'>
+          {/* Default */}
+          <Stack className='gap-2'>
+            <BodySmall className='text-label-muted'>Default</BodySmall>
+            <Input placeholder='Name' />
+          </Stack>
         </Stack>
       </Stack>
     </>
