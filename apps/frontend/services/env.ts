02d4f98084c56f49268199ad6175fe017d06b999
Salief <salieflewis@gmail.com>
2023-09-15 11:10:16 -0400

    config env service

--- apps/frontend/services/env.ts
+++ apps/frontend/services/env.ts
@@ -1,3 +1,5 @@
+import { objectKeys } from '../utils';
+
 interface Env {
   NEXT_PUBLIC_ALCHEMY_KEY: string;
   NEXT_PUBLIC_WALLET_CONNECT_ID: string;
@@ -6,7 +8,7 @@ interface Env {
   NEXT_PUBLIC_BASEMENT_API: string;
 }
 
-const env: Env = {
+export const env: Env = {
   NEXT_PUBLIC_ALCHEMY_KEY: process.env.NEXT_PUBLIC_ALCHEMY_KEY as string,
   NEXT_PUBLIC_WALLET_CONNECT_ID: process.env
     .NEXT_PUBLIC_WALLET_CONNECT_ID as string,
@@ -15,3 +17,18 @@ const env: Env = {
     .NEXT_PUBLIC_WEB3STORAGE_TOKEN as string,
   NEXT_PUBLIC_BASEMENT_API: process.env.NEXT_PUBLIC_BASEMENT_API as string,
 };
+
+const frontendKeys = objectKeys(env).filter((key) =>
+  key.includes('NEXT_PUBLIC')
+);
+
+const missingKeys: Array<string> = [];
+
+frontendKeys.forEach((key) => {
+  if (!env[key]) {
+    missingKeys.push(key);
+  }
+});
+if (missingKeys.length > 0) {
+  throw new Error(`Missing environment variables: ${missingKeys.join(', ')}`);
+}
