c61a595f0e1d9b59b9f25809395c295ac555c952
Salief <salieflewis@gmail.com>
2023-09-19 10:27:31 -0400

    wip, implement button styles

--- apps/frontend/components/client/addToChannel/ChannelModal.tsx
+++ apps/frontend/components/client/addToChannel/ChannelModal.tsx
@@ -1,41 +1,31 @@
-'use client';
+'use client'
 
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  Button,
-  Body,
-  Exit,
-  Stack,
-} from '@river/estuary';
-import { HorizontalNav } from './HorizontalNav';
-import { SearchContainer } from './search/SearchContainer';
-import { Flex } from '@river/estuary';
-import { XIcon } from 'lucide-react';
+import { Dialog, DialogContent, DialogTrigger, Button } from '@river/estuary'
+import { HorizontalNav } from './HorizontalNav'
+import { SearchContainer } from './search/SearchContainer'
+import { Flex } from '@river/estuary'
+import { XIcon } from 'lucide-react'
 
 export function ChannelModal() {
-  const [activeTab, setActiveTab] = useState<string>('Search');
-  const [open, setOpen] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState<string>('Search')
+  const [open, setOpen] = useState<boolean>(false)
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger className='focus-outline:none'>
-        <Button>
-          <Body className='font-medium'>+ Add</Body>
-        </Button>
+      <DialogTrigger className="focus-outline:none">
+        <Button>Add</Button>
       </DialogTrigger>
-      <DialogContent className='flex flex-col border-[0.5px] border-base-border rounded-[14px] h-fit'>
-        <Flex className='items-center px-4'>
+      <DialogContent className="flex flex-col border-[0.5px] border-base-border rounded-[14px] h-fit">
+        <Flex className="items-center px-4">
           <HorizontalNav activeTab={activeTab} setActiveTab={setActiveTab} />
-          <Button onClick={() => setOpen(false)} size='icon'>
+          <Button onClick={() => setOpen(false)} size="icon" shape="circle">
             <XIcon />
           </Button>
         </Flex>
         {activeTab === 'Search' ? <SearchContainer /> : null}
       </DialogContent>
     </Dialog>
-  );
+  )
 }
--- apps/frontend/components/client/addToChannel/search/SearchInput.tsx
+++ apps/frontend/components/client/addToChannel/search/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import {
   Select,
   SelectContent,
@@ -6,29 +6,29 @@ import {
   SelectTrigger,
   SelectValue,
   Flex,
-  SearchIcon,
   Body,
   Label,
   Stack,
-} from '@river/estuary'
+  Button,
+} from '@river/estuary';
+import { SearchIcon } from 'lucide-react';
+import { Input } from '@river/estuary/src/components/Input';
 
-import { Input } from '@river/estuary/src/components/Input'
-
-import { networks } from '../../../../constants'
-import useGetTokenMetadata from '../../../../hooks/useGetTokenMetadata'
-import { Nft } from 'alchemy-sdk'
-import Image from 'next/image'
-import { Ethereum } from '@river/estuary'
+import { networks } from '../../../../constants';
+import useGetTokenMetadata from '../../../../hooks/useGetTokenMetadata';
+import { Nft } from 'alchemy-sdk';
+import Image from 'next/image';
+import { Ethereum } from '@river/estuary';
 
 interface NetworkSelectProps {
-  selectedNetwork: number
-  setSelectedNetwork: React.Dispatch<React.SetStateAction<number>>
+  selectedNetwork: number;
+  setSelectedNetwork: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const getIconPath = (networkId: number) => {
-  const networkName = networks[networkId]
-  return networkName ? '/icons/ethereum.svg' : ''
-}
+  const networkName = networks[networkId];
+  return networkName ? '/icons/ethereum.svg' : '';
+};
 
 function NetworkSelect({
   selectedNetwork,
@@ -37,8 +37,8 @@ function NetworkSelect({
   return (
     <Select onValueChange={(value) => setSelectedNetwork(Number(value))}>
       <SelectTrigger
-        id="networkSelect"
-        className="w-fit h-[40px] border-[1.5px] border-[#DADADA] focus:ring-0"
+        id='networkSelect'
+        className='w-fit h-[40px] border-[1.5px] border-[#DADADA] focus:ring-0'
       >
         <SelectValue>
           <Ethereum />
@@ -47,23 +47,23 @@ function NetworkSelect({
       <SelectContent>
         {Object.entries(networks).map(([networkId, networkName]) => (
           <SelectItem key={networkId} value={networkId}>
-            <Body className="text-label">{networkName}</Body>
+            <Body className='text-label'>{networkName}</Body>
           </SelectItem>
         ))}
       </SelectContent>
     </Select>
-  )
+  );
 }
 
 type SearchInputProps = {
-  searchResults: Nft | undefined
-  setSearchResults: React.Dispatch<React.SetStateAction<Nft | undefined>>
+  searchResults: Nft | undefined;
+  setSearchResults: React.Dispatch<React.SetStateAction<Nft | undefined>>;
   setSearchParams: (params: {
-    network?: number | undefined
-    contractAddress?: string | undefined
-    tokenId?: string | undefined
-  }) => void
-}
+    network?: number | undefined;
+    contractAddress?: string | undefined;
+    tokenId?: string | undefined;
+  }) => void;
+};
 
 const SearchInput = ({
   searchResults,
@@ -71,43 +71,43 @@ const SearchInput = ({
   setSearchParams,
 }: SearchInputProps) => {
   // Change the initial state and state type to number instead of string | null
-  const [network, setNetwork] = useState<number>(1) // defaulting to Ethereum's ID
-  const [contractAddress, setContractAddress] = useState<string>('')
-  const [tokenId, setTokenId] = useState<string>('')
+  const [network, setNetwork] = useState<number>(1); // defaulting to Ethereum's ID
+  const [contractAddress, setContractAddress] = useState<string>('');
+  const [tokenId, setTokenId] = useState<string>('');
 
   const { tokenMetadata, fetchMetadata } = useGetTokenMetadata({
     network: network,
     address: contractAddress,
     tokenId: tokenId,
-  })
+  });
 
   // trigger target metadata fetch
   const handleFetchMetadata = () => {
-    fetchMetadata()
-  }
+    fetchMetadata();
+  };
 
   useEffect(() => {
     if (tokenMetadata) {
-      setSearchResults(tokenMetadata)
+      setSearchResults(tokenMetadata);
     }
-  }, [tokenMetadata])
+  }, [tokenMetadata]);
 
   useEffect(() => {
     setSearchParams({
       network,
       contractAddress,
       tokenId,
-    })
-  }, [network, contractAddress, tokenId])
+    });
+  }, [network, contractAddress, tokenId]);
 
   return (
-    <Flex className="items-center justify-between">
+    <Flex className='items-center justify-between'>
       {/* Network select */}
-      <Stack className="gap-y-1">
+      <Stack className='gap-y-1'>
         <Label
-          htmlFor="networkSelect"
-          className="text-[#7B7B7B] w-fit text-[10px]"
-          id="networkSelect"
+          htmlFor='networkSelect'
+          className='text-[#7B7B7B] w-fit text-[10px]'
+          id='networkSelect'
         >
           Network
         </Label>
@@ -117,55 +117,53 @@ const SearchInput = ({
         />
       </Stack>
       {/* Address input */}
-      <Flex className="flex-col gap-y-1">
+      <Stack className='gap-y-1'>
         <Label
-          htmlFor="addressInput"
-          className="text-[#7B7B7B] w-fit text-[10px]"
+          htmlFor='addressInput'
+          className='text-[#7B7B7B] w-fit text-[10px]'
         >
           Address
         </Label>
         <Input
-          id="addressInput"
-          type="text"
-          placeholder="0xA837b..."
+          id='addressInput'
+          type='text'
+          placeholder='0xA837b...'
           value={contractAddress}
           onChange={(e) => setContractAddress(e.target.value)}
-          className="px-2 w-[256px] h-[40px] border-[1.5px] rounded border-[#DADADA] focus-visible:ring-0"
+          className='px-2 w-[256px] h-[40px] border-[1.5px] rounded border-[#DADADA] focus-visible:ring-0'
         />
-      </Flex>
+      </Stack>
       {/* Token id input */}
-      <Flex className="flex-col gap-y-1">
+      <Stack className='gap-y-1'>
         <Label
-          htmlFor="tokenIdInput"
-          className="text-[#7B7B7B] w-fit text-[10px]"
+          htmlFor='tokenIdInput'
+          className='text-[#7B7B7B] w-fit text-[10px]'
         >
           ID
         </Label>
         <Input
-          id="tokenIdInput"
-          type="text"
-          placeholder="17"
+          id='tokenIdInput'
+          type='text'
+          placeholder='17'
           value={tokenId}
           onChange={(e) => setTokenId(e.target.value)}
-          className="px-2 w-14 h-[40px] border-[1.5px] border-[#DADADA] focus-visible:outline-none focus-visible:ring-0"
+          className='px-2 w-14 h-[40px] border-[1.5px] border-[#DADADA] focus-visible:outline-none focus-visible:ring-0'
         />
-      </Flex>
-      {/* Search button */}
-      <button
-        type="button"
-        className="mt-5 self-center rounded-full bg-[#F2F2F2] hover:bg-[#A8A8A8] disabled:bg-[#F2F2F2] focus:outline-none"
-        onClick={() => {
-          // Handle the search here using network, contractAddress, and tokenId
-          handleFetchMetadata()
-        }}
-      >
-        <SearchIcon
-          width="35"
-          className="fill-[#393939] hover:fill-[#FEFEFE] disabled:fill-[#CACACA]"
-        />
-      </button>
+      </Stack>
+      {/* Search */}
+      <span className='content-end'>
+        <Button
+          size='icon'
+          shape='circle'
+          onClick={() => {
+            handleFetchMetadata(); // Handle search using network, contractAddress, and tokenId
+          }}
+        >
+          <SearchIcon />
+        </Button>
+      </span>
     </Flex>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
