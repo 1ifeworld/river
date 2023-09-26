4b1ea33b0cb06c8605320f7518f93f75f09d552c
Salief <salieflewis@gmail.com>
2023-09-26 00:13:36 -0400

    add testnet badge

--- apps/frontend/components/client/sidebarOrDrawer/Sidebar.tsx
+++ apps/frontend/components/client/sidebarOrDrawer/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Stack, Body } from '@river/estuary'
+import { Stack, Body, Flex, BodySmall, BodyLarge } from '@river/estuary'
 import Link from 'next/link'
 import { Connect } from '../Connect'
 import { AdminChannels } from '../AdminChannels'
@@ -13,20 +13,26 @@ export function Sidebar() {
       <Stack className="justify-between h-full">
         <span>
           <Stack className="gap-2">
-            <span>
-              <Body className="text-label-faint font-medium cursor-default">
-                River
-              </Body>
+            {/* River */}
+            <Flex className="gap-2 items-center">
               <Link href="/">
-                <Body className="text-label hover:underline">Home </Body>
+                <BodyLarge className="text-label font-medium cursor-default hover:text-label/80">
+                  River
+                </BodyLarge>
               </Link>
-            </span>
+              <div className="bg-label rounded-[2px] px-1 py-[0.5px]">
+                <BodySmall className="text-[#FFFFFF] text-[10px] uppercase">
+                  testnet
+                </BodySmall>
+              </div>
+            </Flex>
+            {/* Create Channel */}
             <Link href="/channel">
-              <Body className="text-label-faint hover:text-label-faint/80 font-medium my-2">
+              <Body className="text-label-faint hover:text-label-faint/60 font-medium my-2">
                 Create Channel
               </Body>
             </Link>
-
+            {/* My Channels */}
             {isConnected && <AdminChannels address={address as Hex} />}
           </Stack>
         </span>
