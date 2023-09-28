3171a11758f62994973f2f84999a328024b17bb4
Salief <salieflewis@gmail.com>
2023-09-28 12:49:10 -0400

    add intro

--- apps/estuary-docs/app/page.tsx
+++ apps/estuary-docs/app/page.tsx
@@ -1,3 +1,11 @@
+import { BodyLarge, Stack } from "@river/estuary"
+
 export default function Home() {
-  return;
+  return (
+    <Stack className='gap-3 max-w-prose'>
+      <BodyLarge>Estuary is an opinionated design system built for River.</BodyLarge>
+      <BodyLarge>It is organized into three core areas: <span className="font-bold">Tokens</span>, <span className="font-bold">Elements</span>, and <span className="font-bold">Components</span>.</BodyLarge>
+      <BodyLarge>For more detailed information, navigate to an individual piece to view example implementations and instructions for use.</BodyLarge>
+    </Stack>
+  )
 }
