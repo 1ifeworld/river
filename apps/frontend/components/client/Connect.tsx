a23065247873b57f0fcf7add4146d5adde70370b
Salief <salieflewis@gmail.com>
2023-09-29 10:50:31 -0400

    remove disconnect behavior from auth component on mobile

--- apps/frontend/components/client/Connect.tsx
+++ apps/frontend/components/client/Connect.tsx
@@ -4,11 +4,36 @@ import { type Hex } from 'viem'
 import { Flex, Body, Stack, Button, cn, Debug } from '@river/estuary'
 import { shortenAddress } from '@/utils'
 import { useDisconnect } from 'wagmi'
-import { useGetAddressDisplay } from 'hooks/useGetAddressDisplay'
+import { useGetAddressDisplay, useMediaQuery } from '@/hooks'
 
 function Auth({ address }: { address?: Hex }) {
   const { disconnect } = useDisconnect()
   const { display } = useGetAddressDisplay(address as Hex)
+  const { isMobile } = useMediaQuery()
+
+  if (isMobile) {
+    return (
+      <button type="button">
+        <Flex className="items-center gap-[10px]">
+          <Avatar address={address} size={40} />
+          <Stack className="pr-2 text-left">
+            {display !== shortenAddress(address) ? (
+              <>
+                <Body className="text-label">{display}</Body>
+                <Body className="text-label-muted">
+                  {shortenAddress(address)}
+                </Body>
+              </>
+            ) : (
+              <Body className="text-label-muted">
+                {shortenAddress(address)}
+              </Body>
+            )}
+          </Stack>
+        </Flex>
+      </button>
+    )
+  }
 
   return (
     <button
