90834b2439e061fe7f6baa285f2ec14a1bd67ff8
Salief <salieflewis@gmail.com>
2023-10-26 11:28:01 -0400

    display did if authenticated

--- apps/site/components/client/Header.tsx
+++ apps/site/components/client/Header.tsx
@@ -1,13 +1,20 @@
-import { Flex, Button } from '@/design-system'
+import { Flex, Button, Typography } from '@/design-system'
 import { usePrivy } from '@privy-io/react-auth'
 
 export function Header() {
-  const { login } = usePrivy()
+  const { login, ready, authenticated, user } = usePrivy()
+
+  // Show nothing if user is not authenticated or data is still loading
+  if (!(ready && authenticated) || !user) {
+    return null;
+  }
+
   return (
     <Flex className="w-full items-center justify-end">
-      <Button variant="link" onClick={login}>
-        Login
-      </Button>
+      {authenticated ? <Typography>{user.id}</Typography> :
+        <Button variant="link" onClick={login}>
+          Login
+        </Button>}
     </Flex>
   )
 }
