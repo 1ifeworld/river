8257cf58644264b2c8999bfcf2d92be0477fd3cd
Salief <salieflewis@gmail.com>
2023-10-30 12:25:24 -0400

    add privy wagmi provider, add op goerli const

--- apps/site/app/providers.tsx
+++ apps/site/app/providers.tsx
@@ -1,35 +1,20 @@
 'use client'
 
-import { PrivyProvider } from '@privy-io/react-auth'
-import { ConnectKitProvider } from 'connectkit'
+import { PrivyProviderWrapper } from '@/client'
 import * as React from 'react'
-import { WagmiConfig } from 'wagmi'
-import { config } from '../wagmiConfig'
+import { PrivyWagmiConnector } from '@privy-io/wagmi-connector'
+import { configureChainsConfig } from '../wagmiConfig'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = React.useState(false)
   React.useEffect(() => setMounted(true), [])
   return (
-    <WagmiConfig config={config}>
-      {/* <ConnectKitProvider> */}
-      <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
-        config={{
-          appearance: {
-            showWalletLoginFirst: false,
-            logo: 'https://bafybeibjpp3pfnawvtmkqpemzhcsygj64ime7zngdpdd2evpzmvdespi7a.ipfs.nftstorage.link/river_logo.svg',
-          },
-          loginMethods: ['email'],
-          embeddedWallets: {
-            createOnLogin: 'users-without-wallets',
-            requireUserPasswordOnCreate: false,
-            noPromptOnSignature: true,
-          },
-        }}
-      >
+    <PrivyProviderWrapper>
+      <PrivyWagmiConnector wagmiChainsConfig={configureChainsConfig}>
+        {/* <ConnectKitProvider> */}
         {mounted && children}
-      </PrivyProvider>
-      {/* </ConnectKitProvider> */}
-    </WagmiConfig>
+        {/* </ConnectKitProvider> */}
+      </PrivyWagmiConnector>
+    </PrivyProviderWrapper>
   )
 }
