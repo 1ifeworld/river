e0d62849bcd0e58374c45e4602aaa9d3b6f6f8e3
Salief <salieflewis@gmail.com>
2023-11-06 14:05:23 -0500

    add scaffolding for username registration

--- apps/site/components/client/UsernameDialog.tsx
+++ apps/site/components/client/UsernameDialog.tsx
@@ -22,6 +22,10 @@ import {
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
+import { useAlchemyContext } from '@/context'
+import { useEffect, useState } from 'react'
+import { Hex } from 'viem'
+import { operator } from '@/constants'
 
 const FormSchema = z.object({
   username: z.string().min(2, {
@@ -38,7 +42,78 @@ export function UsernameDialog({ open }: { open: boolean }) {
     },
   })
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {}
+  // id
+  // name
+  // owner
+
+  const [smartAccountAddress, setSmartAccountAddress] = useState<Hex>()
+
+  const alchemyProvider = useAlchemyContext()
+
+  console.log('Alchemy provider', alchemyProvider)
+
+  useEffect(() => {
+    const fetchAddress = async () => {
+      const address = await alchemyProvider?.getAddress()
+      setSmartAccountAddress(address as Hex)
+    }
+    fetchAddress()
+  }, [alchemyProvider])
+
+  interface nameRegistration {
+    id: string
+    name: string
+    owner: string
+  }
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    const registerField: nameRegistration = {
+      id: '10',
+      name: `${data.username}.sbvrsv.eth`,
+      // owner: String(smartAccountAddress),
+      owner: String(operator)
+    }
+    const headers = getCORSHeaders()
+
+    // Helper function to set CORS headers
+    function getCORSHeaders() {
+      return new Headers({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '86400',
+      })
+    }
+    // await fetch('https://server.talktomenice.workers.dev/set', {
+    //   method: 'POST',
+    //   headers: headers,
+    //   body: JSON.stringify(registerField),
+    // })
+    await fetch('https://server.talktomenice.workers.dev/set', {
+      method: 'POST',
+      // headers: headers,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'no-cors',
+      body: JSON.stringify(registerField),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        if (data.success) {
+          console.log('Name set successfully')
+        } else {
+          alert(data.error)
+          console.error('Error:', data.error)
+        }
+      })
+      .catch((error) => {
+        alert('wahhh')
+        console.error('Fetch error:', error)
+      })
+  }
+
   return (
     <Dialog open={open}>
       <DialogContent className="sm:max-w-[425px]">
--- apps/site/constants/operator.ts
+++ apps/site/constants/operator.ts
@@ -1,4 +1,7 @@
 import { privateKeyToAccount } from 'viem/accounts'
 import { type Hash } from 'viem'
 
-export const operator = privateKeyToAccount(process.env._PRIVATE_KEY as Hash)
+export const operator = privateKeyToAccount(
+  '0x5b11374e776da448d75bf65030d7cece94dfa419896cab14bdc0fa813dbd06a7',
+  // process.env._PRIVATE_KEY as Hash
+)
