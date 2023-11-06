1520a42350ea0e24790c74e11d7c9b60b1f7cbc9
0xTranqui <thinkandchill11@gmail.com>
2023-11-06 18:13:55 -0500

    oops quick fix

--- apps/site/constants/operator.ts
+++ apps/site/constants/operator.ts
@@ -1,5 +1,4 @@
 import { privateKeyToAccount } from 'viem/accounts'
 import { type Hash } from 'viem'
 
-// export const operator = privateKeyToAccount(process.env._PRIVATE_KEY as Hash)
-export const operator = privateKeyToAccount('0x5b11374e776da448d75bf65030d7cece94dfa419896cab14bdc0fa813dbd06a7' as Hash)
+export const operator = privateKeyToAccount(process.env._PRIVATE_KEY as Hash)
