194a337a2836587f9ccf0f9420919cce91ffc413
jawndiego <jawndiego@gmail.com>
2023-11-07 12:44:06 -0500

    all IdRegistry events

--- apps/delta/src/IdRegistry.ts
+++ apps/delta/src/IdRegistry.ts
@@ -1,5 +1,80 @@
 import { ponder } from "@/generated";
 
+ponder.on("IdRegistry:Attest", async ({event, context}) => {
+  const {IdRegistry} = context.entities;
+  const {id, attestor} = event.params
+
+  await IdRegistry.create({
+    id: `420/${id}/${attestor}`,
+    data: {
+      userId: id,
+      attestor: attestor,
+    }
+  })
+
+});
+
+ponder.on("IdRegistry:RevokeAttestation", async ({event, context}) => {
+  const {IdRegistry} = context.entities;
+  const {id, attestor} = event.params
+
+  await IdRegistry.create({
+    id: `420/${id}/${attestor}`,
+    data: {
+      userId: id,
+      attestor: attestor,
+    }
+  })
+
+})
+
+ponder.on("IdRegistry:TransferCancelled", async ({event, context}) => {
+  const {IdRegistry} = context.entities;
+  const {from, to, id} = event.params
+
+  await IdRegistry.create({
+    id: `420/${from}/${to}/${id}`,
+    data: {
+      from: from,
+      to: to,
+      userId: id,
+    }
+  })
+
+})
+
+ponder.on("IdRegistry:TransferComplete", async ({event, context}) => {
+
+  const {IdRegistry} = context.entities;
+  const {from, to, id} = event.params
+
+  await IdRegistry.create({
+    id: `420/${from}/${to}/${id}`,
+    data: {
+      from: from,
+      to: to,
+      userId: id,
+    }
+  })
+
+})
+
+ponder.on("IdRegistry:TransferInitiated", async ({event, context}) => {
+  const {IdRegistry} = context.entities;
+  const {from, to, id} = event.params
+
+  await IdRegistry.create({
+    id: `420/${from}/${to}/${id}`,
+    data: {
+      from: from,
+      to: to,
+      userId: id,
+    }
+  })
+
+})
+
+
 ponder.on("IdRegistry:Register", async ({ event, context }) => {
   
   const { IdRegistry } = context.entities;
