8eed7d979b0b5a9278c3f0de499e054b9e6aee3a
jawndiego <jawndiego@gmail.com>
2023-11-07 12:49:38 -0500

    river validator crud update

--- apps/delta/src/RiverValidator.ts
+++ apps/delta/src/RiverValidator.ts
@@ -1,6 +1,6 @@
 import { ponder } from '@/generated'
 
-ponder.on('RiverValidatorV1:Validate', async ({ event, context }) => {
+ponder.on("RiverValidatorV1:Validate", async ({ event, context }) => {
   const { RiverValidatorV1 } = context.entities
 
   const { id, status } = event.params
@@ -13,3 +13,16 @@ ponder.on('RiverValidatorV1:Validate', async ({ event, context }) => {
         status: status },
   })
 })
+
+ponder.on("RiverValidatorV1:OperatorUpdated", async ({ event, context }) => {
+  const { RiverValidatorV1 } = context.entities
+
+  const { operator } = event.params
+
+  await RiverValidatorV1.create({
+    // chain // validatorAddress // userId
+    id: `${420}/${event.transaction.from}/${operator}`,
+    data: { 
+       operator: operator},
+  }) 
+}) 
