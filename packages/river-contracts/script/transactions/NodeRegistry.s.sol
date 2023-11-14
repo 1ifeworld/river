eb9ae84cbfe0f1f269588c8c26143d218d7c12a5
jawndiego <jawndiego@gmail.com>
2023-11-14 15:00:33 -0500

    lots of transactions script for node registry

--- packages/river-contracts/script/transactions/NodeRegistry.s.sol
+++ packages/river-contracts/script/transactions/NodeRegistry.s.sol
@@ -2,42 +2,55 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-
 import {NodeRegistry} from "imp/NodeRegistry.sol";
 
 contract NodeRegistryScript is Script {
+    NodeRegistry nodeRegistry =
+        NodeRegistry(0x5FbDB2315678afecb367f032d93F642f64180aa3);
+    bytes32 constant PUB_SCHEMA =
+        0xF36F2F0432F99EA34A360F154CEA9D1FAD45C7319E27ADED55CC0D28D0924068;
+    bytes32 constant CHANNEL_SCHEMA =
+        0x08B83A3AFF9950D7F88522AC4A172BD8405BE30B0D3B416D42FD73C30AC27C9F;
 
-    NodeRegistry nodeRegistry = NodeRegistry(0xF26F07040922992DF06091235A50872e31C85Ab8);
-    uint256 constant MOCK_USER_ID = 1;
-    bytes32 constant PUB_SCHEMA = 0xF36F2F0432F99EA34A360F154CEA9D1FAD45C7319E27ADED55CC0D28D0924068;
-    bytes32 constant CHANNEL_SCHEMA = 0x08B83A3AFF9950D7F88522AC4A172BD8405BE30B0D3B416D42FD73C30AC27C9F;
-    
+    uint256 private nonce = 0; // Unique data modifier
+
     function setUp() public {}
 
     function run() public {
-        /* Load private key */
         bytes32 privateKeyBytes = vm.envBytes32("PRIVATE_KEY");
         uint256 deployerPrivateKey = uint256(privateKeyBytes);
-        /* Start function transmission */
         vm.startBroadcast(deployerPrivateKey);
 
-        // registerPubNode();
-
-        // updatePubNode();
-
-        // registerChannelNode();
-
-        updateChannelNode();
+        recursiveOperation(1000);
 
         vm.stopBroadcast();
-        /* End function transmission */
     }
 
+    function recursiveOperation(uint256 num_ops) internal {
+
+        for (uint256 i; i < num_ops; i++) {
+            registerPubNode();
+            // updatePubNode();
+            // registerChannelNode();
+            // updateChannelNode();
+        }
+    }
+
+    // Random number generator for MOCK_USER_ID
+    function random() internal view returns (uint256) {
+        return
+            (uint256(
+                keccak256(
+                    abi.encodePacked(block.timestamp, block.number, msg.sender)
+                )
+            ) % 10) + 1;
+    }
 
     // PUBLICATION HELPERS
 
     function registerPubNode() public {
-        // prep register publication message data
+        uint256 mockUserId = random(); // Randomly generated user ID
+
         bytes[] memory messages = new bytes[](2);
         uint256[] memory admins = new uint256[](1);
         admins[0] = 1;
@@ -46,20 +59,21 @@ contract NodeRegistryScript is Script {
         members[1] = 3;
         messages[0] = abi.encode(101, abi.encode(admins, members));
         messages[1] = abi.encode(201, abi.encode("yourIpfsStringHere"));
-        nodeRegistry.register(MOCK_USER_ID, PUB_SCHEMA, messages);
+        nodeRegistry.register(mockUserId, PUB_SCHEMA, messages);
     }
 
     function updatePubNode() public {
-        // prep update publication -- set Uri message data
+        uint256 mockUserId = random();
         bytes[] memory messages = new bytes[](1);
         messages[0] = abi.encode(201, abi.encode("newIpfsString"));
-        nodeRegistry.update(MOCK_USER_ID, 5, messages);
+        nodeRegistry.update(mockUserId, nonce, messages);
     }
 
     // CHANNEL HELPERS
 
     function registerChannelNode() public {
-        // prep register channel + add first item message data
+        uint256 mockUserId = random();
+
         bytes[] memory messages = new bytes[](3);
         uint256[] memory admins = new uint256[](1);
         admins[0] = 1;
@@ -67,28 +81,34 @@ contract NodeRegistryScript is Script {
         members[0] = 2;
         members[1] = 3;
         messages[0] = abi.encode(101, abi.encode(admins, members));
-        messages[1] = abi.encode(301, abi.encode("channelIpfsUri"));        
-        messages[2] = abi.encode(302, abi.encode(
-            10,                     // chainId
-            4,                      // id
-            address(nodeRegistry),  // pointer
-            true                    // hasId
-        ));      
-        nodeRegistry.register(MOCK_USER_ID, CHANNEL_SCHEMA, messages);
-    }    
+        messages[1] = abi.encode(301, abi.encode("channelIpfsUri"));
+        messages[2] = abi.encode(
+            302,
+            abi.encode(
+                10, // chainId
+                nonce, // id using nonce for uniqueness
+                address(nodeRegistry), // pointer
+                true // hasId
+            )
+        );
+        nodeRegistry.register(mockUserId, CHANNEL_SCHEMA, messages);
+    }
 
     function updateChannelNode() public {
-        // prep update channel -- add new item message data
+        uint256 mockUserId = random(); // Randomly generated user ID
+
         bytes[] memory messages = new bytes[](1);
-        messages[0] = abi.encode(302, abi.encode(
-            18,                     // chainId
-            9,                      // id
-            address(nodeRegistry),  // pointer
-            true                    // hasId
-        ));      
-        // userId, nodeId, messages
-        nodeRegistry.update(MOCK_USER_ID, 9, messages);
-    }    
+        messages[0] = abi.encode(
+            302,
+            abi.encode(
+                18, // chainId
+                nonce, // id using nonce
+                address(nodeRegistry), // pointer
+                true // hasId
+            )
+        );
+        nodeRegistry.update(mockUserId, nonce, messages);
+    }
 }
 
 // ======= DEPLOY SCRIPTS =====
