e402a0c98d04db4ee95e9680cd7e82644d49030d
0xTranqui <thinkandchill11@gmail.com>
2023-11-14 12:21:28 -0500

    got update messages working as well

--- apps/delta/src/nodeRegistry/Register.ts
+++ apps/delta/src/nodeRegistry/Register.ts
@@ -14,7 +14,7 @@ import {
 } from "offchain-schema";
 
 ponder.on("NodeRegistry:Register", async ({ event, context }) => {
-    const { Node, Message, Publication, Channel, Item, RiverValidatorV1, Register } = context.entities;
+    const { Node, Message, Publication, Channel, Item } = context.entities;
     const { sender, userId, schema, nodeId, messages } = event.params;
     
     console.log(`Node $${nodeId} Registered`);  
@@ -51,7 +51,7 @@ ponder.on("NodeRegistry:Register", async ({ event, context }) => {
         // -- have to preform access checks because techicanlly the initializing 100 message
         // -- could have set admin/member params that dont allow the userId to 
         // -- process the message they are trying to
-        console.log("messages.length", messages.length)
+        console.log("register messages.length", messages.length)
         for (let i = 0; i < messages.length; ++i) {            
             // decode msg. will be null if invalid data, so add logic check after
             const decodedMsg = decodeMessage000({encodedMsg: messages[i]})
@@ -62,6 +62,7 @@ ponder.on("NodeRegistry:Register", async ({ event, context }) => {
                     id: `420/${event.transaction.from}/${event.transaction.hash}/${event.log.logIndex}/${i}`,
                     data: {
                         sender: sender,
+                        userId: userId,
                         msgType: decodedMsg.msgType,
                         msgBody: decodedMsg.msgBody                          
                     }
@@ -105,80 +106,135 @@ ponder.on("NodeRegistry:Register", async ({ event, context }) => {
                                 uri: decoded.uri
                             }
                         });    
-                    } else if (decodedMsg.msgType == BigInt(302)) {
-                        const decoded = decodeChannel302({msgBody: decodedMsg.msgBody})
-                        if (decoded) {                        
-                            // create channel if it doesnt already exist by this point
-                            //      it might already exist, but if it does, we dont want to update the uri
-                            await Channel.upsert({
-                                id: `420/${event.transaction.from}/${schema}/${nodeId}`,
-                                create: {},
-                                update: {}
-                            });    
-                            // create item and associate it with the parent
-                            await Item.create({
-                                id: `420/${event.transaction.from}/${schema}/${nodeId}/${event.transaction.hash}/${event.log.logIndex}`,
-                                data: {
-                                    chainId: decoded.chainId,
-                                    targetId: decoded.id,
-                                    target: decoded.pointer,
-                                    hasId: decoded.hasId,
-                                    channel: `420/${event.transaction.from}/${schema}/${nodeId}`,
-                                },
-                            });                            
-                        }
-                    }   
-                }
+                    }
+                } else if (decodedMsg.msgType == BigInt(302)) {
+                    const decoded = decodeChannel302({msgBody: decodedMsg.msgBody})
+                    if (decoded) {                        
+                        // create channel if it doesnt already exist by this point
+                        //      it might already exist, but if it does, we dont want to update the uri
+                        await Channel.upsert({
+                            id: `420/${event.transaction.from}/${schema}/${nodeId}`,
+                            create: {},
+                            update: {}
+                        });    
+                        // create item and associate it with the parent
+                        console.log("Decoded channel", decoded)
+
+                        await Item.create({
+                            id: `420/${event.transaction.from}/${schema}/${nodeId}/${event.transaction.hash}/${event.log.logIndex}`,
+                            data: {
+                                chainId: decoded.chainId,
+                                targetId: decoded.id,
+                                target: decoded.pointer,
+                                hasId: decoded.hasId,
+                                channel: `420/${event.transaction.from}/${schema}/${nodeId}`,
+                            },
+                        });                            
+                    }
+                }                   
             }
         }
     }
 })
-  
-    // const decodedNodeRegistrationStruct = decodeNodeRegistrationData({
-    //   data: data,
-    // });
-  
-    // // exit crud process if node registration decoding returns null
-    // if (decodedNodeRegistrationStruct == null) return;
-  
-    // const decodedAccessControlData = decodeAdminWithMembersData({
-    //   msgType: decodedNodeRegistrationStruct.msgType,
-    //   msgBody: decodedNodeRegistrationStruct.msgBody,
-    // });
-  
-    // // exit crud process if access control decoding returns null
-    // if (decodedAccessControlData == null) return;
-  
-    // // write decoded data to database
-    // await Node.create({
-    //   id: `420/${event.transaction.from}/${nodeId}`,
-    //   data: {
-    //     sender: sender,
-    //     nodeId: nodeId,
-    //     userId: decodedNodeRegistrationStruct.userId,
-    //     schema: decodedNodeRegistrationStruct.schema,
-    //     msgType: decodedNodeRegistrationStruct.msgType,
-    //     msgBody: decodedNodeRegistrationStruct.msgBody,
-    //     nodeAdmin: decodedAccessControlData.admin,
-    //     nodeMembers: decodedAccessControlData.members as bigint[],
-    //   },
-    // });
-  
-    // if (decodedNodeRegistrationStruct.schema === publicationSchema.toLowerCase()) {
-    //   await Publication.create({
-    //     id: `420/${publicationSchema}/${nodeId}`,
-    //     data: {
-    //       // this is set to empty since currently we do not allow for setting
-    //       //    the uri of a Publication on registration
-    //       uri: "",
-    //     },
-    //   });
-    // } else if (decodedNodeRegistrationStruct.schema === channelSchema.toLowerCase()) {
-    //   await Channel.create({
-    //     id: `420/${channelSchema}/${nodeId}`,
-    //     data: undefined,
-    //   });
-    // }
+
+ponder.on("NodeRegistry:Update", async ({ event, context }) => {
+    const { Node, Message, Publication, Channel, Item } = context.entities;
+    const { sender, userId, nodeId, messages } = event.params;
+    
+    console.log(`Node $${nodeId} Updated`);  
+
+    // the future version is
+    // check if sender is either the owner or a delegate address for this id
+    // const targetUser = await RiverValidatorV1.findUnique({
+    //     id: `420/${addresses.riverValidatorV1.opGoerli}/${userId}`
+    // })
+
+    // all events will go through for now
+    const validUser = true; 
+
+    // check that target node exists
+    const targetNode = await Node.findUnique({
+        id: `420/${event.transaction.from}/${nodeId}`
+    })    
+
+    // Check if user is valid
+    if (validUser && targetNode) {
+        
+        // this schema will be used for targeting correct nodes in crud
+        const schema = targetNode.schema
+    
+        console.log("update messages.length", messages.length)
+        for (let i = 0; i < messages.length; ++i) {            
+            // decode msg. will be null if invalid data, so add logic check after
+            const decodedMsg = decodeMessage000({encodedMsg: messages[i]})
+            // check if decodedMsg was null
+            if (decodedMsg && isValidMessageId(decodedMsg.msgType)) {                    
+                // record every properly decoded msg
+                await Message.create({
+                    id: `420/${event.transaction.from}/${event.transaction.hash}/${event.log.logIndex}/${i}`,
+                    data: {
+                        sender: sender,
+                        userId: userId,
+                        msgType: decodedMsg.msgType,
+                        msgBody: decodedMsg.msgBody                          
+                    }
+                })                               
+                // process msgBody depending on msgType
+                if (decodedMsg.msgType === BigInt(101)) {
+                    // attempt to decode access control body
+                    const decoded = decodeAccess101({msgBody: decodedMsg.msgBody})
+                    // if successful, enter crud logic, if not exit
+                    if (decoded) {                        
+                        await Node.update({
+                            id: `420/${event.transaction.from}/${nodeId}`,
+                            data: {
+                                nodeAdmin: decoded?.admins as bigint[],
+                                nodeMembers: decoded?.members as bigint[]
+                            },
+                        });    
+                    } else {
+                        // this exists the entire crud loop if no access controll set
+                        return
+                    }
+                } else if (decodedMsg.msgType === BigInt(201)) {
+                    const decoded = decodePublication201({msgBody: decodedMsg.msgBody})
+                    if (decoded) {                                                    
+                        await Publication.update({
+                            id: `420/${event.transaction.from}/${schema}/${nodeId}`,
+                            data: {
+                                uri: decoded.uri
+                            },
+                        });    
+                    }
+                } else if (decodedMsg.msgType == BigInt(301)) {
+                    const decoded = decodeChannel301({msgBody: decodedMsg.msgBody})
+                    if (decoded) {                        
+                        await Channel.update({
+                            id: `420/${event.transaction.from}/${schema}/${nodeId}`,
+                            data: {
+                                uri: decoded.uri
+                            },
+                        });    
+                    }
+                } else if (decodedMsg.msgType == BigInt(302)) {
+                    const decoded = decodeChannel302({msgBody: decodedMsg.msgBody})
+                    if (decoded) {                        
+                        await Item.create({
+                            id: `420/${event.transaction.from}/${schema}/${nodeId}/${event.transaction.hash}/${event.log.logIndex}`,
+                            data: {
+                                chainId: decoded.chainId,
+                                targetId: decoded.id,
+                                target: decoded.pointer,
+                                hasId: decoded.hasId,
+                                channel: `420/${event.transaction.from}/${schema}/${nodeId}`,
+                            },
+                        });                            
+                    }
+                }                   
+            }
+        }
+    }
+})
 
 
 // ponder.on("NodeRegistry:RegisterNode", async ({ event, context }) => {
--- packages/river-contracts/script/transactions/NodeRegistry.s.sol
+++ packages/river-contracts/script/transactions/NodeRegistry.s.sol
@@ -10,16 +10,34 @@ contract NodeRegistryScript is Script {
     NodeRegistry nodeRegistry = NodeRegistry(0xF26F07040922992DF06091235A50872e31C85Ab8);
     uint256 constant MOCK_USER_ID = 1;
     bytes32 constant PUB_SCHEMA = 0xF36F2F0432F99EA34A360F154CEA9D1FAD45C7319E27ADED55CC0D28D0924068;
+    bytes32 constant CHANNEL_SCHEMA = 0x08B83A3AFF9950D7F88522AC4A172BD8405BE30B0D3B416D42FD73C30AC27C9F;
     
     function setUp() public {}
 
     function run() public {
+        /* Load private key */
         bytes32 privateKeyBytes = vm.envBytes32("PRIVATE_KEY");
         uint256 deployerPrivateKey = uint256(privateKeyBytes);
-
+        /* Start function transmission */
         vm.startBroadcast(deployerPrivateKey);
 
-        // prep message data
+        // registerPubNode();
+
+        // updatePubNode();
+
+        // registerChannelNode();
+
+        updateChannelNode();
+
+        vm.stopBroadcast();
+        /* End function transmission */
+    }
+
+
+    // PUBLICATION HELPERS
+
+    function registerPubNode() public {
+        // prep register publication message data
         bytes[] memory messages = new bytes[](2);
         uint256[] memory admins = new uint256[](1);
         admins[0] = 1;
@@ -28,11 +46,49 @@ contract NodeRegistryScript is Script {
         members[1] = 3;
         messages[0] = abi.encode(101, abi.encode(admins, members));
         messages[1] = abi.encode(201, abi.encode("yourIpfsStringHere"));
-
         nodeRegistry.register(MOCK_USER_ID, PUB_SCHEMA, messages);
-
-        vm.stopBroadcast();
     }
+
+    function updatePubNode() public {
+        // prep update publication -- set Uri message data
+        bytes[] memory messages = new bytes[](1);
+        messages[0] = abi.encode(201, abi.encode("newIpfsString"));
+        nodeRegistry.update(MOCK_USER_ID, 5, messages);
+    }
+
+    // CHANNEL HELPERS
+
+    function registerChannelNode() public {
+        // prep register channel + add first item message data
+        bytes[] memory messages = new bytes[](3);
+        uint256[] memory admins = new uint256[](1);
+        admins[0] = 1;
+        uint256[] memory members = new uint256[](2);
+        members[0] = 2;
+        members[1] = 3;
+        messages[0] = abi.encode(101, abi.encode(admins, members));
+        messages[1] = abi.encode(301, abi.encode("channelIpfsUri"));        
+        messages[2] = abi.encode(302, abi.encode(
+            10,                     // chainId
+            4,                      // id
+            address(nodeRegistry),  // pointer
+            true                    // hasId
+        ));      
+        nodeRegistry.register(MOCK_USER_ID, CHANNEL_SCHEMA, messages);
+    }    
+
+    function updateChannelNode() public {
+        // prep update channel -- add new item message data
+        bytes[] memory messages = new bytes[](1);
+        messages[0] = abi.encode(302, abi.encode(
+            18,                     // chainId
+            9,                      // id
+            address(nodeRegistry),  // pointer
+            true                    // hasId
+        ));      
+        // userId, nodeId, messages
+        nodeRegistry.update(MOCK_USER_ID, 9, messages);
+    }    
 }
 
 // ======= DEPLOY SCRIPTS =====
