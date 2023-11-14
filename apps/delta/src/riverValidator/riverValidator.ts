8340200e7efe06e76bb6021ee70898c58e13d861
jawndiego <jawndiego@gmail.com>
2023-11-14 17:20:54 -0500

    updated ponder, new ponder config, adding back other contract events

---
+++ apps/delta/src/riverValidator/riverValidator.ts
@@ -0,0 +1,33 @@
+// import { ponder } from '@/generated'
+
+// ponder.on("RiverValidatorV1:Validate", async ({ event, context }) => {
+//   const { RiverValidatorV1 } = context.entities
+
+//   const { id, status } = event.params
+
+//   await RiverValidatorV1.upsert({
+//     // chain // validatorAddress // userId
+//     id: `${420}/${event.transaction.from}/${id}`,
+//     create: { 
+//         userId: id, 
+//         status: status 
+//       },
+//    update: {
+//         status: status 
+//   }
+//     })
+// })
+
+// ponder.on("RiverValidatorV1:OperatorUpdated", async ({ event, context }) => {
+//   const { RiverValidatorV1 } = context.entities
+
+//   const { operator } = event.params
+
+//   await RiverValidatorV1.create({
+//     // chain // validatorAddress // userId
+//     id: `${420}/${event.transaction.from}/${operator}`,
+//     data: { 
+//        operator: operator
+//       },
+//   }) 
+// }) 
