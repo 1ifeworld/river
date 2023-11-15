eafb09a49916cab6be3a00479931cf4dcf3cc343
0xTranqui <thinkandchill11@gmail.com>
2023-11-15 14:48:28 -0500

    import pub schema from scrypt"

--- apps/site/gql/requests/getUserPublications.ts
+++ apps/site/gql/requests/getUserPublications.ts
@@ -1,8 +1,5 @@
 import sdk from '../client'
-
-// TODO: This should get moved to our scrypt package as a constant.
-const publicationSchema =
-  '0xf36f2f0432f99ea34a360f154cea9d1fad45c7319e27aded55cc0d28d0924068'
+import { publicationSchema } from 'scrypt'
 
 export async function getUserPublications({ userId }: { userId: bigint }) {
   const publications = await sdk.UserPublications({
