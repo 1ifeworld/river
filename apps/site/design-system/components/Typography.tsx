904672981883111e289569fc7ca27801a5f1063d
Salief <salieflewis@gmail.com>
2023-11-16 13:33:03 -0500

    adjust default font sizes

--- apps/site/design-system/components/Typography.tsx
+++ apps/site/design-system/components/Typography.tsx
@@ -3,24 +3,12 @@ import * as React from 'react'
 
 import { cn } from '@/design-system'
 
-const typographyVariants = cva('text-foreground', {
+const typographyVariants = cva('text-foreground tracking-tight', {
   variants: {
     variant: {
-      h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
-      h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
-      h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
-      h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
-      h5: 'scroll-m-20 text-lg font-semibold tracking-tight',
-      h6: 'scroll-m-20 text-base font-semibold tracking-tight',
-      p: 'leading-7 [&:not(:first-child)]:mt-6',
-      blockquote: 'mt-6 border-l-2 pl-6 italic',
-      ul: 'my-6 ml-6 list-disc [&>li]:mt-2',
-      inlineCode:
-        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
-      lead: 'text-xl text-muted-foreground',
-      largeText: 'text-lg font-semibold',
-      smallText: 'text-sm font-medium leading-none',
-      mutedText: 'text-sm text-muted-foreground',
+      h1: 'text-2xl',
+      h2: 'text-xl',
+      p: 'text-base leading-6',
     },
   },
 })
@@ -33,18 +21,7 @@ const variantElementMap: Record<
 > = {
   h1: 'h1',
   h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-  h5: 'h5',
-  h6: 'h6',
   p: 'p',
-  blockquote: 'blockquote',
-  inlineCode: 'code',
-  largeText: 'div',
-  smallText: 'small',
-  lead: 'p',
-  mutedText: 'p',
-  ul: 'ul',
 }
 
 type Element = keyof JSX.IntrinsicElements
