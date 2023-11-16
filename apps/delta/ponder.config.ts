5b734e9db8030bd13b62507f303922cd0c7fc312
0xTranqui <thinkandchill11@gmail.com>
2023-11-16 00:25:57 -0500

    small thing"

--- apps/delta/ponder.config.ts
+++ apps/delta/ponder.config.ts
@@ -14,7 +14,7 @@ export const config: Config = {
     {
       name: 'optimism-goerli',
       chainId: 420,
-      transport: http('https://opt-goerli.g.alchemy.com/v2/FAXyCsRGoMoPTbpLvGUcRyp8qZDTBMcU'),
+      transport: http(process.env.PONDER_RPC_URL_420),
     },
     // {
     //   name: 'arbitrum-goerli',
