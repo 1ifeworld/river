28c67caac12ae6b3e5824d672bf386597065d5f0
Salief <salieflewis@gmail.com>
2023-11-16 15:34:46 -0500

    add the batch flag to transports

--- apps/site/config/publicClient.ts
+++ apps/site/config/publicClient.ts
@@ -3,6 +3,9 @@ import { opGoerliViem } from '@/constants'
 
 const transport = http(
   `https://opt-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`,
+  {
+    batch: true,
+  },
 )
 
 export const publicClient = createPublicClient({
