0869157970080467cbd7fc5def5cd7b686bee6d6
jawndiego <jawndiego@gmail.com>
2023-11-27 18:22:31 -0500

    type fix in username

--- apps/site/components/server/Username.tsx
+++ apps/site/components/server/Username.tsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from '@/design-system'
 import { getUsername } from '@/lib'
 
-export async function Username({ id }: { id: number }) {
+export async function Username({ id }: { id: bigint }) {
   const username = await getUsername({ id: id })
 
   return <Typography>{username.username}</Typography>
