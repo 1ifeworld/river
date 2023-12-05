1577d13b6409cbb0215dbc97e243926c1e3c1bcd
Salief <salieflewis@gmail.com>
2023-12-05 10:41:00 -0500

    remove force dynamic route segment option

--- apps/site/app/[username]/page.tsx
+++ apps/site/app/[username]/page.tsx
@@ -1,8 +1,6 @@
 import { Stack } from '@/design-system'
 import { ActivityBanner, ActivityChannels, ActivityItems } from '@/server'
 
-export const dynamic = 'force-dynamic'
-
 export default async function User({
   params,
 }: {
