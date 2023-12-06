a22711a1270ebf6f3dbf63b93b3aa05050d63c79
0xTranqui <thinkandchill11@gmail.com>
2023-12-06 17:04:50 -0500

    oo a bit mofre

--- apps/metadata/api/index.ts
+++ apps/metadata/api/index.ts
@@ -1,18 +1,3 @@
-import express from 'express'
-import { setRouter, getRouter } from '@/services'
-import dotenv from 'dotenv'
+import app from "../index"
 
-dotenv.config()
-
-const PORT = process.env.PORT ?? 8080
-
-const app = express()
-app.use(express.json()) // Add this line
-
-app.use('/set', setRouter)
-
-app.use('/get', getRouter)
-
-app.listen(PORT)
-
-console.log(`Listening on port ${PORT}...`)
+export default app
---
+++ apps/metadata/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { setRouter, getRouter } from '@/services'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT ?? 8080
+
+const app = express()
+app.use(express.json()) // Add this line
+
+app.use('/set', setRouter)
+
+app.use('/get', getRouter)
+
+app.listen(PORT)
+
+console.log(`Listening on port ${PORT}...`)
+
+export default app
