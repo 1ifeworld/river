282adf8992bd49480686f5f2c5244483214f3e97
0xTranqui <thinkandchill11@gmail.com>
2023-12-07 01:27:26 -0500

    it works yay so updating delta

--- apps/delta/src/utils/sendMetadataRequest.ts
+++ apps/delta/src/utils/sendMetadataRequest.ts
@@ -1,29 +1,3 @@
-// // Function to send a set request to the metadata server
-// export async function sendMetadataRequest(cid: string) {
-//   const metadataServerUrl = process.env.METADATA_SERVER_URL; // URL of your metadata server
-
-//   if (!metadataServerUrl) return;
-
-//   try {
-//     const response = await fetch(`${metadataServerUrl}/set`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ cid }),
-//     });
-    
-//     const data = await response.json();
-//     console.log("Metadata request successful:", data);
-//     return data; // Return the data
-//   } catch (error) {
-//     console.error("Metadata request failed:", error);
-//     throw error; // You can rethrow the error or return a specific error response
-//   }
-// }
-
-
-
 // Function to send a set request to the metadata server
 export function sendMetadataRequest(cid: string) {
   const metadataServerUrl = process.env.METADATA_SERVER_URL; // URL of your metadata server
