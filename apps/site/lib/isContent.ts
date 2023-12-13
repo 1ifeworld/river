d6cc4f49a8ae492db4ab49637b5e2562269aed94
Salief <salieflewis@gmail.com>
2023-12-13 11:41:07 -0500

    fix content checker

--- apps/site/lib/isContent.ts
+++ apps/site/lib/isContent.ts
@@ -1,13 +1,7 @@
 export function isVideo({ mimeType }: { mimeType: string }) {
-<<<<<<< HEAD
-  return ['video/mp4', 'video/webm', 'video/ogg', 'video/avi'].includes(
-    mimeType,
-  )
-=======
   return ["video/mp4", "video/webm", "video/ogg", "video/avi", "video/quicktime"].includes(
     mimeType
   );
->>>>>>> main
 }
 
 export function isImage({ mimeType }: { mimeType: string }) {
