8926bb3b35e568ffa4a990a09e8ed6544360ef4b
jawndiego <jawndiego@gmail.com>
2023-12-13 15:22:38 -0500

    further resolves

--- apps/site/lib/isContent.ts
+++ apps/site/lib/isContent.ts
@@ -1,23 +1,16 @@
 export function isVideo({ mimeType }: { mimeType: string }) {
-<<<<<<< HEAD
-  return ['video/mp4', 'video/webm', 'video/ogg', 'video/avi'].includes(
-    mimeType,
-  )
-=======
+
   return ["video/mp4", "video/webm", "video/ogg", "video/avi", "video/quicktime"].includes(
     mimeType
   );
->>>>>>> main
 }
 
 export function isImage({ mimeType }: { mimeType: string }) {
   return ['image/jpeg', 'image/png'].includes(mimeType)
-<<<<<<< HEAD
 }
 
 export function isGLB(file: File) {
   const extension = file.name.split('.').pop()?.toLowerCase()
   return extension === 'glb' || extension === 'gltf'
-=======
->>>>>>> main
+
 }
