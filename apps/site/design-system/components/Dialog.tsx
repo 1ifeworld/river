0e4dcd5f9e1612e7390dad284277bb39f3dda61a
Salief <salieflewis@gmail.com>
2023-12-24 17:14:24 -0500

    rm animations

--- apps/site/design-system/components/Dialog.tsx
+++ apps/site/design-system/components/Dialog.tsx
@@ -3,7 +3,7 @@
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import * as React from 'react'
 
-import { Typography, cn } from '@/design-system'
+import { cn } from '@/design-system'
 
 const Dialog = DialogPrimitive.Root
 
@@ -20,7 +20,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'fixed inset-0 z-50 bg-border/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'fixed inset-0 z-50 bg-border/50',
       className,
     )}
     {...props}
@@ -37,7 +37,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-11/12 md:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 py-5 border border-secondary-foreground bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+        'fixed left-[50%] top-[50%] z-50 grid w-11/12 md:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 py-5 border border-secondary-foreground bg-background',
         className,
       )}
       {...props}
