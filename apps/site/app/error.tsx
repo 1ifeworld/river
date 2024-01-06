225b0cc3a3515cfe5a11a18d8b8e065791bec0ee
Salief <salieflewis@gmail.com>
2024-01-06 13:16:39 -0500

    add index error boundry

---
+++ apps/site/app/error.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Stack, Button, Typography } from '@/design-system'
+
+export default function IndexError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <Stack className="gap-5 justify-center items-center h-[calc(100dvh-72px)]">
+      <div className="text-center">
+        <Typography variant="h2">Something went wrong</Typography>
+        <Typography className="text-red-500">{error.message}</Typography>
+      </div>
+      <Button
+        variant="link"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Please try again
+      </Button>
+    </Stack>
+  )
+}
