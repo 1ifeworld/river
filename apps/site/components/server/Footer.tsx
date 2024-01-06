0237e8cbb5070db07dfe180386c5726406307132
Salief <salieflewis@gmail.com>
2024-01-06 11:26:26 -0500

    adjust bottom padding for footer comp

--- apps/site/components/server/Footer.tsx
+++ apps/site/components/server/Footer.tsx
@@ -2,7 +2,7 @@ import { Typography, Flex } from '@/design-system'
 
 export function Footer() {
   return (
-    <Flex className="gap-2 w-full justify-end pt-3">
+    <Flex className="gap-2 w-full justify-end py-3">
       <Typography className="text-muted-foreground items-center">
         <span className="text-[14px] align-middle">&#169;</span> 2024 ·{' '}
         <a
