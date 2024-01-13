908438da574ec9a68bbc612d1912b057a7f324bf
jawndiego <jawndiego@gmail.com>
2024-01-13 18:31:40 -0500

    batch works. ux is not nice

--- apps/site/components/client/Dropzone.tsx
+++ apps/site/components/client/Dropzone.tsx
@@ -1,57 +1,121 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
-import { w3sUpload } from '@/lib'
+import {
+  w3sUpload,
+  determineContentType,
+  isAudio,
+  isGLB,
+  isPdf,
+  isText,
+  isVideo,
+  processCreatePubPost,
+  sendToDb,
+  uploadToMux,
+  MetadataObject
+} from '@/lib'
 import { Typography, Toast, Flex, Stack, Button } from '@/design-system'
-import { newItemSchema, MAX_FILE_SIZE, ACCEPTED_ITEM_MIME_TYPES } from '@/lib'
+import { MAX_FILE_SIZE, ACCEPTED_ITEM_MIME_TYPES } from '@/lib'
 import { toast } from 'sonner'
+import { useUserContext } from '@/context'
+import { useParams } from 'next/navigation'
+import { useWeb3Storage } from '@/hooks'
 
 export function Dropzone({
   acceptMultipleFiles,
 }: { acceptMultipleFiles: boolean }) {
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([])
+  const { signMessage, userId: targetUserId } = useUserContext()
+  const params = useParams()
+  const { client } = useWeb3Storage()
+
   const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    if (uploadedFiles.length === 0 && acceptedFiles.length > 0) {
+      setUploadedFiles([acceptedFiles[0]])
+    }
+
     for (const file of acceptedFiles) {
-      const formData = new FormData()
-      formData.append('file', file)
-      const { cid } = await w3sUpload(formData)
+      const uploadedFileName = file.name || 'unnamed'
+      const contentType = determineContentType(file)
+
+      const uploadedFileCid = await client?.uploadFile(file)
+      const contentTypeKey =
+        isVideo({ mimeType: contentType }) || isAudio({ mimeType: contentType })
+          ? 2
+          : isPdf({ mimeType: contentType }) || isGLB(file) || isText(file)
+          ? 1
+          : 0
+
+      const animationUri =
+        contentTypeKey === 2 || contentTypeKey === 1 ? uploadedFileCid : ''
+      const imageUri = contentTypeKey === 0 ? uploadedFileCid : ''
+
+      const metadataObject: MetadataObject = {
+        name: uploadedFileName,
+        description: 'Dynamic metadata based on timestamp',
+        image: imageUri?.toString() as string,
+        animationUri: animationUri?.toString() as string,
+      }
+
+      const pubUri = await client?.uploadFile(
+        new Blob([JSON.stringify(metadataObject)], { type: 'application/json' })
+      )
+
+      let muxAssetId = ''
+      let muxPlaybackId = ''
+
+      if (contentTypeKey === 2) {
+        const muxUploadResult = await uploadToMux(
+          contentType,
+          animationUri?.toString() as string
+        )
+        muxAssetId = muxUploadResult.muxAssetId
+        muxPlaybackId = muxUploadResult.muxPlaybackId
+      }
+
+      await sendToDb({
+        key: pubUri?.toString() as string,
+        value: {
+          ...metadataObject,
+          contentType: contentType,
+          muxAssetId: muxAssetId,
+          muxPlaybackId: muxPlaybackId,
+        },
+      })
+
+      if (signMessage && targetUserId) {
+        await processCreatePubPost({
+          pubUri: pubUri?.toString() as string,
+          targetChannelId: BigInt(params.id as string),
+          targetUserId: BigInt(targetUserId),
+          privySignMessage: signMessage,
+        })
+      }
+
       toast.custom((t) => (
         <Toast>
-          <Typography>{`Successfully uploaded ${file.name}`}</Typography>
+          {'Successfully uploaded '}
+          <Typography>
+            <span className="font-bold">{file.name}</span>
+          </Typography>
         </Toast>
       ))
     }
-  }, [])
-
-  const [showFileList, setShowFileList] = React.useState<boolean>(false)
-  const [filesToUpload, setFilesToUpload] = React.useState<File[]>([])
-
-  const onDropValidated = React.useCallback((acceptedFiles: File[]) => {
-    setShowFileList(true)
-    setFilesToUpload(acceptedFiles)
-  }, [])
-
-  const onDropRejected = React.useCallback(() => {
-    toast.custom(() => (
-      <Toast>
-     <span className="font-bold">
-        {"Only image files are accepted."}
-        </span>
-      </Toast>
-    ))
-    setShowFileList(false)
-  }, [])
+  }, [uploadedFiles, client, signMessage, targetUserId, params.id])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop: onDropValidated,
-    onDropRejected,
+    onDrop,
     multiple: acceptMultipleFiles,
     maxSize: MAX_FILE_SIZE,
-    accept: ACCEPTED_ITEM_MIME_TYPES, 
+    accept: ACCEPTED_ITEM_MIME_TYPES,
   })
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
       <AddItem isDragActive={isDragActive} />
+      {uploadedFiles.length > 0 && (
+        <FileListToast files={uploadedFiles} uploading={false} />
+      )}
     </div>
   )
 }
@@ -70,3 +134,17 @@ function AddItem({ isDragActive }: { isDragActive: boolean }) {
     </Flex>
   )
 }
+
+function FileListToast({ files, uploading }: { files: File[], uploading: boolean }) {
+  return (
+    <Toast>
+      <Typography variant="h2">{uploading ? 'Uploading Files...' : 'Uploaded Files:'}</Typography>
+      <ul>
+        {files.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
+    </Toast>
+  )
+}
+
