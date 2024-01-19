03eebeaff8821105f9180f3cc6cb3e64df22a2a6
jawndiego <jawndiego@gmail.com>
2024-01-18 21:58:08 -0500

    prep for live deploy

--- apps/site/lib/api.ts
+++ apps/site/lib/api.ts
@@ -1,6 +1,6 @@
 export async function w3sUpload(body: FormData, authToken: string | null) {
-  // const res = await fetch('https://w3s.up.railway.app/w3s', {
-  const res = await fetch('http://localhost:3000/w3s', {
+    // const res = await fetch('http://localhost:3000/w3s', {
+  const res = await fetch('https://w3s.up.railway.app/w3s', {
     method: 'POST',
     headers: authToken ? { Authorization: `Bearer ${authToken}` } : undefined,
     body,
@@ -15,8 +15,8 @@ export async function w3sUpload(body: FormData, authToken: string | null) {
 }
 
 export async function uploadToMux(body: string, authToken: string | null) {
-  // const res = await fetch('https://w3s.up.railway.app/mux', {
-  const res = await fetch('http://localhost:3000/mux/upload', {
+    // const res = await fetch('http://localhost:3000/mux/upload', {
+  const res = await fetch('https://w3s.up.railway.app/mux', {
     method: 'POST',
     headers: authToken ? { Authorization: `Bearer ${authToken}` } : undefined,
     body,
