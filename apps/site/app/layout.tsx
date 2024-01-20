0e34e9683ac76fdbdee8a387577a62e3301917b8
Salief <salieflewis@gmail.com>
2024-01-20 17:28:04 -0500

    add saturn integration

--- apps/site/app/layout.tsx
+++ apps/site/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'sonner'
 import '../styles/globals.css'
 import { fragmentMono } from './fonts/fonts'
 import { Providers } from './providers/providers'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'River',
@@ -33,6 +34,10 @@ export default function RootLayout({
           <Analytics />
         </Providers>
       </body>
+      <Script
+        async
+        src="https://saturn.tech/widget.js#integration=14b09943-4822-45b7-892d-a0150f577c33"
+      />
     </html>
   )
 }
