fdf0163c8272d0ee551dd516348646f7a5cc984a
Salief <salieflewis@gmail.com>
2024-01-21 09:40:51 -0500

    bump biome to latest, add dropzone components

--- apps/site/components/client/Dropzone.tsx
+++ apps/site/components/client/Dropzone.tsx
@@ -40,8 +40,8 @@ export function Dropzone({
           isAudio({ mimeType: contentType })
             ? 2
             : isPdf({ mimeType: contentType }) || isGLB(file) || isText(file)
-            ? 1
-            : 0
+              ? 1
+              : 0
 
         const animationUri =
           contentTypeKey === 2 || contentTypeKey === 1 ? cid : ''
