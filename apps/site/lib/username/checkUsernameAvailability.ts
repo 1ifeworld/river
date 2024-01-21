3b5c257e5174d23d1422efee7c2ff3b7d9e0eaa7
jawndiego <jawndiego@gmail.com>
2024-01-21 17:43:16 -0500

    owner check to address issue#346

--- apps/site/lib/username/checkUsernameAvailability.ts
+++ apps/site/lib/username/checkUsernameAvailability.ts
@@ -2,6 +2,10 @@ export interface CheckUsernameResponse {
   exists: boolean
 }
 
+export interface CheckOwnerReponse {
+  exists: boolean
+}
+
 export async function checkUsernameAvailability(
   username: string,
 ): Promise<CheckUsernameResponse> {
@@ -20,3 +24,22 @@ export async function checkUsernameAvailability(
     throw error
   }
 }
+
+export async function checkOwnerHasId(owner: string,): 
+Promise<CheckOwnerReponse>{
+  try {
+    const response = await fetch(
+      `https://server.talktomenice.workers.dev/id/${`${owner}`}`,
+    )
+    if (response.status === 200) {
+      return { exists: true }
+    } else if(response.status === 404) {
+      return { exists: false }
+    }
+    throw new Error('Unexpected response from the server')
+  } catch (error) {
+    console.error('Error checking for owner:', error)
+    throw error
+  }
+}
+
