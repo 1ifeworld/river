9c0b87c0eed22bdd7b45feed40db6bedcd59bc50
Salief <salieflewis@gmail.com>
2024-01-23 13:35:20 -0500

    rename and update endpoints

--- apps/site/lib/api.ts
+++ apps/site/lib/api.ts
@@ -1,10 +1,8 @@
 export async function w3sUpload(body: FormData, authToken: string | null) {
-    const res = await fetch('https://w3s-service-w3s-service-pr-13.up.railway.app/w3s', {
+  const res = await fetch('https://river-media-service.up.railway.app/w3s', {
     method: 'POST',
     headers: authToken ? { Authorization: `Bearer ${authToken}` } : undefined,
     body,
-    // TODO: Add credentials check
-    //   credentials: 'same-origin',
   })
   if (!res.ok) {
     console.error('Could not upload file', await res.text())
@@ -14,11 +12,14 @@ export async function w3sUpload(body: FormData, authToken: string | null) {
 }
 
 export async function uploadToMux(body: string, authToken: string | null) {
-    const res = await fetch('https://w3s.up.railway.app/mux/upload', {
-    method: 'POST',
-    headers: authToken ? { Authorization: `Bearer ${authToken}` } : undefined,
-    body,
-  })
+  const res = await fetch(
+    'https://river-media-service.up.railway.app/mux/upload',
+    {
+      method: 'POST',
+      headers: authToken ? { Authorization: `Bearer ${authToken}` } : undefined,
+      body,
+    },
+  )
   if (!res.ok) {
     console.error('Could not process video', await res.text())
     throw new Error('Could not process video')
