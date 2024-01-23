8080f8330e8cd6da1b91cb3f2fb8da758543f0bb
Salief <salieflewis@gmail.com>
2024-01-23 14:22:52 -0500

    update index route

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,17 +1,15 @@
 import { Grid, Stack } from '@/design-system'
-import { type Channel, getAllChannels } from '@/gql'
-import { getChannelMetadata } from '@/lib'
-import { ChannelCard, Footer } from '@/server'
+import { getAllPublications } from '@/gql'
+import { ItemCard, Footer } from '@/server'
 
 export default async function Home() {
-  const { channels } = await getAllChannels()
-  const { metadata } = await getChannelMetadata(channels)
+  const { publications } = await getAllPublications()
 
   return (
     <Stack className="justify-between h-[100dvh] pt-12 md:pt-20">
-      <Grid className="grid-cols-1 md:grid-cols-[repeat(auto-fill,_minmax(272px,_1fr))] gap-2 py-3">
-        {channels.map((channel: Channel, index: number) => (
-          <ChannelCard key={index} channel={channel} metadata={metadata} />
+      <Grid className="grid-cols-1 md:grid-cols-[repeat(auto-fill,_minmax(255px,_1fr))] gap-5 py-[30px]">
+        {publications.map((publication, index: number) => (
+          <ItemCard key={index} publication={publication} />
         ))}
       </Grid>
       <Footer />
