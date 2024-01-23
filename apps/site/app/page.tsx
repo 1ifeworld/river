c0769929a7d9834130c2824b99c9b29e937aa6d7
Salief <salieflewis@gmail.com>
2024-01-23 17:51:11 -0500

    two column grid on mobile

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <Stack className="justify-between h-[100dvh] pt-12 md:pt-20">
-      <Grid className="grid-cols-1 md:grid-cols-[repeat(auto-fill,_minmax(255px,_1fr))] gap-5 py-[30px]">
+      <Grid className="grid-cols-2 md:grid-cols-[repeat(auto-fill,_minmax(255px,_1fr))] gap-5 py-[30px]">
         {publications.map((publication, index: number) => (
           <ItemCard key={index} publication={publication} />
         ))}
