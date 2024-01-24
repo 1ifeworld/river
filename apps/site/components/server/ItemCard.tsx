1434c7c3c507f752f0e234d7cc6430ecff63440c
Salief <salieflewis@gmail.com>
2024-01-24 11:03:44 -0500

    add item link

--- apps/site/components/server/ItemCard.tsx
+++ apps/site/components/server/ItemCard.tsx
@@ -4,6 +4,7 @@ import { getUsername, type MediaAssetObject, pinataUrlFromCid } from '@/lib'
 import { unixTimeConverter } from '@/utils'
 import { kv } from '@vercel/kv'
 import Image from 'next/image'
+import Link from 'next/link'
 import { GenericThumbnailLarge } from '@/server'
 
 export async function ItemCard({
@@ -38,10 +39,14 @@ export async function ItemCard({
       </Stack>
       <Stack className="gap-y-[10px]">
         <div>
-          {/* TODO: Add link out to item in the context of its channel */}
-          <Typography className="truncate">
-            {itemMetadata?.name ?? 'untitled'}
-          </Typography>
+          <Link
+            href={`/item/${publication.id}`}
+            className="hover:underline underline-offset-2 transition-all"
+          >
+            <Typography className="truncate">
+              {itemMetadata?.name ?? 'untitled'}
+            </Typography>
+          </Link>
           <Flex className="items-center">
             <Typography className="text-secondary-foreground">
               {username ?? ''}
