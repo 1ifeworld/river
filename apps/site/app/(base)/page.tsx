a0e259c4f6918b7a0eb337e4ecb4be6c9a3a0966
Salief <salieflewis@gmail.com>
2024-01-25 09:23:47 -0500

    update routing for item cards to use ref ids

--- apps/site/app/(base)/page.tsx
+++ apps/site/app/(base)/page.tsx
@@ -1,14 +1,14 @@
 import { Grid } from '@/design-system'
-import { getAllPublications } from '@/gql'
+import { getAllReferences } from '@/gql'
 import { ItemCard } from '@/server'
 
 export default async function Home() {
-  const { publications } = await getAllPublications()
+  const { references } = await getAllReferences()
 
   return (
     <Grid className="grid-cols-2 md:grid-cols-[repeat(auto-fill,_minmax(255px,_1fr))] gap-5 py-[30px]">
-      {publications.map((publication, index: number) => (
-        <ItemCard key={index} publication={publication} />
+      {references.map((reference, index: number) => (
+        <ItemCard key={index} reference={reference} />
       ))}
     </Grid>
   )
