9334d0400cf853bb44daa1c3432efd825ff58f8d
Salief <salieflewis@gmail.com>
2024-01-25 09:28:01 -0500

    ignore type err

--- apps/site/app/(base)/page.tsx
+++ apps/site/app/(base)/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
   return (
     <Grid className="grid-cols-2 md:grid-cols-[repeat(auto-fill,_minmax(255px,_1fr))] gap-5 py-[30px]">
       {references.map((reference, index: number) => (
+        // @ts-ignore
         <ItemCard key={index} reference={reference} />
       ))}
     </Grid>
