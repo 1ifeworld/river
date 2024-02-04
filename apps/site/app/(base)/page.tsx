7c3e0b1aecf1608e178efa0438be5b56b7316a55
Salief <salieflewis@gmail.com>
2024-02-04 15:23:46 -0500

    layout adjustments for new routing

--- apps/site/app/(base)/page.tsx
+++ apps/site/app/(base)/page.tsx
@@ -6,11 +6,9 @@ import { Typography } from '@/design-system'
 export default async function Home() {
   const { adds } = await getAllAdds()
 
-  if (!adds || !adds.items || adds?.items?.length == 0) {
+  if (!adds || !adds.items || adds?.items?.length === 0) {
     return <Typography>No items added yet</Typography>
   }
-
-  // adds.items[0].
 
   return (
     <Grid className="grid-cols-2 md:grid-cols-[repeat(auto-fill,_minmax(255px,_1fr))] gap-5 py-[30px]">
---
+++ apps/site/app/channel/[id]/page.tsx
@@ -0,0 +1,53 @@
+import { Stack, Typography, Flex } from '@/design-system'
+import { getChannelWithId } from '@/gql'
+import { getAddsMetadata } from '@/lib'
+import {
+  ChannelBanner,
+  ChannelItems,
+  ChannelDetails,
+  MarqueeWrapper,
+  RecentChannels,
+} from '@/server'
+
+export default async function Channel({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const { channel } = await getChannelWithId({
+    id: params.id,
+  })
+
+  if (!channel) {
+    return <Typography>This is not a valid channel</Typography>
+  }
+
+  // @ts-ignore
+  const { metadata } = await getAddsMetadata(channel?.adds?.items)
+
+  return (
+    <section>
+      <div className="hidden md:block fixed top-[38px] z-50 w-full">
+        <MarqueeWrapper />
+      </div>
+      <Flex className="px-5 pt-[70px] md:pt-[110px]">
+        <div className="hidden md:w-[19%] md:block">
+          <RecentChannels params={params} />
+        </div>
+        <div className="w-full md:w-[78%]">
+          <Stack>
+            <Stack className="gap-y-[45px]">
+              {/* @ts-ignore */}
+              <ChannelBanner channel={channel} />
+              {/* @ts-ignore */}
+              <ChannelItems channel={channel} metadata={metadata} />
+            </Stack>
+            {/* @ts-ignore */}
+            <ChannelDetails channel={channel} />
+          </Stack>
+        </div>
+        <div className="hidden md:w-[3%] md:block">{}</div>
+      </Flex>
+    </section>
+  )
+}
