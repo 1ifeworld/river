078ef12763aa2aeead404a5239536b75133f617d
jawndiego <jawndiego@gmail.com>
2024-02-05 12:48:51 -0500

    env rename

--- apps/site/config/publicClient.ts
+++ apps/site/config/publicClient.ts
@@ -1,7 +1,7 @@
 import { http, createPublicClient } from 'viem'
 import { arbitrumNova } from './customChainConfig'
 
-const transport = http(process.env.RPC_URL)
+const transport = http(process.env.NOVA_RPC_URL)
 
 export const publicClient = createPublicClient({
   chain: arbitrumNova,
