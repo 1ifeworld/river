b344be946fcb2f29ac71ee110801048a6474f2a5
jawndiego <jawndiego@gmail.com>
2024-02-05 01:09:33 -0500

    custom chain config

--- apps/site/config/customChainConfig.ts
+++ apps/site/config/customChainConfig.ts
@@ -11,12 +11,12 @@ export const arbitrumNova = defineChain({
   },
   rpcUrls: {
     default: {
-       http: [`${process.env.RPC_URL}`],
-       webSocket: [`${process.env.RPC_URL_WSS}`],
-},
+       http: ['https://g.w.lavanet.xyz:443/gateway/arbn/rpc-http/86cbe35dfe0686d90306955304fb64d1'],
+       webSocket: ['wss://g.w.lavanet.xyz:443/gateway/arbn/rpc/86cbe35dfe0686d90306955304fb64d1'],
+    },
     public: {
-      http: [`${process.env.RPC_URL}`],
-      webSocket: [`${process.env.RPC_URL_WSS}`],
+      http: ['https://g.w.lavanet.xyz:443/gateway/arbn/rpc-http/86cbe35dfe0686d90306955304fb64d1'],
+      webSocket: ['wss://g.w.lavanet.xyz:443/gateway/arbn/rpc/86cbe35dfe0686d90306955304fb64d1'],
     },
   },
   blockExplorers: {
@@ -27,11 +27,44 @@ export const arbitrumNova = defineChain({
   },
   contracts: {
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
       blockCreated: 1746963,
     },
   },
 })
+
+// export const arbitrumNova = defineChain({
+//   id: 42170,
+//   name: 'arbitrumNova',
+//   network: 'arbitrumNova',
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: 'Ether',
+//     symbol: 'ETH',
+//   },
+//   rpcUrls: {
+//     default: {
+//        http: [`${process.env.RPC_URL}`],
+//        webSocket: [`${process.env.RPC_URL_WSS}`],
+// },
+//     public: {
+//       http: [`${process.env.RPC_URL}`],
+//       webSocket: [`${process.env.RPC_URL_WSS}`],
+//     },
+//   },
+//   blockExplorers: {
+//     default: {
+//       name: 'Explorer',
+//       url: 'https://nova.arbiscan.io/',
+//     },
+//   },
+//   contracts: {
+//     multicall3: {
+//       address: '0xca11bde05977b3631167028862be2a173976ca11',
+//       blockCreated: 1746963,
+//     },
+//   },
+// })
 
 // export const river_j5bpjduqfv = defineChain({
 //   id: 36912,
