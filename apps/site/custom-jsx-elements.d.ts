fd967cb32a626bd5ba21a22b07dca817e17078ec
jawndiego <jawndiego@gmail.com>
2024-02-07 17:09:54 -0500

    global namespace

--- apps/site/custom-jsx-elements.d.ts
+++ apps/site/custom-jsx-elements.d.ts
@@ -1,20 +1,8 @@
-declare global {
+import { ModelViewerElement } from "@google/model-viewer"
+
+export declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'model-viewer': {
-        ref?: RefObject<HTMLElement>;
-        src?: string;
-        ios?: boolean;
-        alt?: string;
-        ar?: boolean;
-        progress?: boolean;
-        autoRotate?: boolean;
-        preload?: boolean;
-        autoplay?: boolean;
-        loading?: string;
-        cameraControls?: boolean;
-      } & React.HTMLAttributes<HTMLElement>;
+      "model-viewer": React.DetailedHTMLProps<Partial<ModelViewerElement>>;
     }
-  }
-}
-export {}
+  } }
