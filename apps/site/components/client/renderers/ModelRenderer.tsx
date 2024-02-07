cd0b1d87cff6bd2a61e2a01a6945711d0afc92c1
jawndiego <jawndiego@gmail.com>
2024-02-07 13:25:36 -0500

    testing script on mobile

--- apps/site/components/client/renderers/ModelRenderer.tsx
+++ apps/site/components/client/renderers/ModelRenderer.tsx
@@ -1,15 +1,17 @@
 'use client'
 
-import React, { useEffect } from "react";
-
+import React, { useEffect } from "react"
+import Script from "next/script"
 interface ModelRendererProps {
   src: string;
 }
 
 const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
-  useEffect(() => {
-    import('@google/model-viewer').catch(console.error)
-  }, [])
+  // useEffect(() => {
+  //   import('@google/model-viewer').catch(console.error)
+  // }, [])
+
+  <Script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"></Script>
 
   return (
     <>
