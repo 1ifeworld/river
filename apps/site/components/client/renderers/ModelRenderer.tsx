033358bf2935bb2e95119d8cf63bf0f9616430e2
jawndiego <jawndiego@gmail.com>
2024-02-07 14:14:43 -0500

    proper decoder reference

--- apps/site/components/client/renderers/ModelRenderer.tsx
+++ apps/site/components/client/renderers/ModelRenderer.tsx
@@ -1,32 +1,75 @@
 'use client'
 
-import React, { useEffect } from "react"
+// import React, { useEffect } from "react"
+// import Script from "next/script"
+// interface ModelRendererProps {
+//   src: string;
+// }
+
+// const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
+//   // useEffect(() => {
+//   //   import('@google/model-viewer').catch(console.error)
+//   // }, [])
+
+
+//   return (
+//     <>
+//       <Script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js" />
+
+//       <model-viewer
+//         src={src}
+//         ios-src={src}
+//         ios
+//         // ar
+//         autoplay  
+//         // ar-modes="webxr scene-viewer" 
+//         progress
+//         camera-controls 
+//         loading ='eager'
+//         // preload
+//         touch-action="pan-y" 
+//         style={{ width: '100%', height: '100%' }}
+//       ></model-viewer>
+//     </>
+//   )
+// }
+
+// export default ModelRenderer
+
+
 import Script from "next/script"
+import React from "react"
+
 interface ModelRendererProps {
-  src: string;
+  src: string
 }
 
 const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
-  // useEffect(() => {
-  //   import('@google/model-viewer').catch(console.error)
-  // }, [])
-
-
+  const dracoDecoderLocation = './'
   return (
     <>
-      <Script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js" />
-
+      <Script 
+        src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"
+        type="module"
+        onLoad={() => {
+          const scriptContent = `
+            self.ModelViewerElement = self.ModelViewerElement || {}
+            self.ModelViewerElement.dracoDecoderLocation = '${dracoDecoderLocation}'
+          `
+          if (typeof window !== "undefined") {
+            const script = document.createElement('script')
+            script.textContent = scriptContent
+            document.head.appendChild(script)
+          }
+        }}
+      />
       <model-viewer
         src={src}
-        ios-src={src}
-        ios
-        // ar
+        loading="eager"
+        ar
         autoplay  
-        // ar-modes="webxr scene-viewer" 
-        progress
+        ar-modes="webxr scene-viewer"
         camera-controls 
-        loading ='eager'
-        // preload
         touch-action="pan-y" 
         style={{ width: '100%', height: '100%' }}
       ></model-viewer>
