99474c1e54309c83d88a02e8eebc1357708abe2d
0xTranqui <thinkandchill11@gmail.com>
2024-02-07 16:17:57 -0500

    class name nit

--- apps/site/components/server/Thumbnails.tsx
+++ apps/site/components/server/Thumbnails.tsx
@@ -32,7 +32,10 @@ export function GenericThumbnailLarge({
 
 export function GenericThumbnailSmall({ text, className }: { text: string, className?: string }) {
   return (
-    <Stack className={cn('bg-[#E9E9E9] justify-center items-center w-[38px] h-[38px]')}>
+    <Stack className={cn(
+      'bg-[#E9E9E9] justify-center items-center w-[38px] h-[38px]',
+      className
+    )}>
       <Typography className="text-secondary-foreground text-[10px]">
         {truncateText(extractFileExtension(text), 5, false)}
       </Typography>
