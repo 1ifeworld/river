6d809323cbfb1d38061fd8d22fbd47fa1c647584
jawndiego <jawndiego@gmail.com>
2024-02-07 18:13:32 -0500

    import tesT

--- apps/site/components/client/renderers/ModelRenderer.tsx
+++ apps/site/components/client/renderers/ModelRenderer.tsx
@@ -1,6 +1,15 @@
 'use client'
 
-import React, { useEffect } from "react"
+import React, { useEffect, CSSProperties } from "react"
+import { ModelViewerElement } from '@google/model-viewer'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'model-viewer': Partial<ModelViewerElement>
+    }
+  }
+}
 
 interface ModelRendererProps {
   src: string
@@ -8,27 +17,60 @@ interface ModelRendererProps {
 
 const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
   useEffect(() => {
-    import("@google/model-viewer").catch(console.error)
+    import('@google/model-viewer').catch(console.error)
   }, [])
+
+  const style: CSSProperties = {
+    width: '100%',
+    height: '100%',
+  }
 
   return (
     <>
       <model-viewer
         src={src}
         autoplay
-        progress
-        camera-controls
+        camera-controls 
         touch-action="pan-y"
-        style={{ width: "100%", height: "100%" }}
-        // ios-src={src}
-        // ios
-        // ar
-        // ar-modes="webxr scene-viewer"
-        // loading ='eager'
-        // preload
-      ></model-viewer>
+        style={{ width: '100%', height: '100%' } as any}
+        ></model-viewer>
     </>
   )
 }
 
 export default ModelRenderer
+
+
+// working example
+// import React, { useEffect } from "react"
+
+// interface ModelRendererProps {
+//   src: string
+// }
+
+// const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
+//   useEffect(() => {
+//     import("@google/model-viewer").catch(console.error)
+//   }, [])
+
+//   return (
+//     <>
+//       <model-viewer
+//         src={src}
+//         autoplay
+//         progress
+//         camera-controls
+//         touch-action="pan-y"
+//         style={{ width: "100%", height: "100%" }}
+//         // ios-src={src}
+//         // ios
+//         // ar
+//         // ar-modes="webxr scene-viewer"
+//         // loading ='eager'
+//         // preload
+//       ></model-viewer>
+//     </>
+//   )
+// }
+
+// export default ModelRenderer
--- apps/site/custom-jsx-elements.d.ts
+++ apps/site/custom-jsx-elements.d.ts
@@ -1,20 +1,20 @@
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'model-viewer': {
-        ref?: RefObject<HTMLElement>
-        src?: string
-        ios?: boolean
-        alt?: string
-        ar?: boolean
-        progress?: boolean
-        autoRotate?: boolean
-        preload?: boolean
-        autoplay?: boolean
-        loading?: string
-        cameraControls?: boolean
-      } & React.HTMLAttributes<HTMLElement>
-    }
-  }
-}
-export {}
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       'model-viewer': {
+//         ref?: RefObject<HTMLElement>
+//         src?: string
+//         ios?: boolean
+//         alt?: string
+//         ar?: boolean
+//         progress?: boolean
+//         autoRotate?: boolean
+//         preload?: boolean
+//         autoplay?: boolean
+//         loading?: string
+//         cameraControls?: boolean
+//       } & React.HTMLAttributes<HTMLElement>
+//     }
+//   }
+// }
+// export {}
