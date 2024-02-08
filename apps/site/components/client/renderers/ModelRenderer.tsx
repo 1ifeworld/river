0e9c9392337a37954e0f23218ef320c47253d7ca
jawndiego <jawndiego@gmail.com>
2024-02-08 10:49:17 -0500

    model renderer cleanup

--- apps/site/components/client/renderers/ModelRenderer.tsx
+++ apps/site/components/client/renderers/ModelRenderer.tsx
@@ -39,38 +39,3 @@ const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
 }
 
 export default ModelRenderer
-
-
-// working example
-// import React, { useEffect } from "react"
-
-// interface ModelRendererProps {
-//   src: string
-// }
-
-// const ModelRenderer: React.FC<ModelRendererProps> = ({ src }) => {
-//   useEffect(() => {
-//     import("@google/model-viewer").catch(console.error)
-//   }, [])
-
-//   return (
-//     <>
-//       <model-viewer
-//         src={src}
-//         autoplay
-//         progress
-//         camera-controls
-//         touch-action="pan-y"
-//         style={{ width: "100%", height: "100%" }}
-//         // ios-src={src}
-//         // ios
-//         // ar
-//         // ar-modes="webxr scene-viewer"
-//         // loading ='eager'
-//         // preload
-//       ></model-viewer>
-//     </>
-//   )
-// }
-
-// export default ModelRenderer
