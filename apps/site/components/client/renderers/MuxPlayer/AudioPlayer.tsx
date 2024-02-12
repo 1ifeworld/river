df768b05a9b5d35b3d36239784b384c06b254ce7
jawndiego <jawndiego@gmail.com>
2024-02-12 12:12:36 -0500

    audio player

--- apps/site/components/client/renderers/MuxPlayer/AudioPlayer.tsx
+++ apps/site/components/client/renderers/MuxPlayer/AudioPlayer.tsx
@@ -1,25 +1,92 @@
-import MuxPlayer from '@mux/mux-player-react'
-import styles from './Player.module.css'
+import React, { useState, useRef } from "react"
+import { Flex } from "@/design-system"
+import MuxAudio from "@mux/mux-audio-react"
+import styles from "./Player.module.css"
 
 export function AudioPlayer({ playbackId }: { playbackId: string }) {
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+  const [progress, setProgress] = useState(0)
+
+  // Toggle play/pause
+  const togglePlayPause = () => {
+    const audio = audioRef.current
+    if (audio) {
+      if (isPlaying) {
+        audio.pause()
+      } else {
+        audio.play()
+      }
+      setIsPlaying(!isPlaying)
+    }
+  }
+
+  // Format time to mm:ss
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60)
+    const seconds = Math.floor(time % 60)
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`
+  }
+
+  // Event handler for time updates
+  const handleTimeUpdate = () => {
+    const audio = audioRef.current
+    if (audio) {
+      const progressValue = (audio.currentTime / audio.duration) * 100
+      setCurrentTime(audio.currentTime)
+      setProgress(progressValue) // Update progress state
+    }
+  }
+
+  // Event handler to set the duration once the audio metadata is loaded
+  const handleLoadedMetadata = () => {
+    const audio = audioRef.current
+    if (audio) {
+      setDuration(audio.duration)
+    }
+  }
+
+  const handleProgressBarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement // Cast the event target to the specific element type
+    const newTime = (target.valueAsNumber / 100) * duration
+    if (audioRef.current) {
+      // Check that audioRef.current is not null
+      audioRef.current.currentTime = newTime
+      setCurrentTime(newTime)
+    }
+  }
 
   return (
-    
-    <div className="flex justify-center items-center h-screen">
-    
-      <div className="w-full max-w-lg px-30">
-        <MuxPlayer
-          playbackId={playbackId}
-          streamType="on-demand"
-          autoPlay={false}
-          audio
-          className={styles.muxPlayer}
-          secondaryColor='#F3F4F6'
-          primaryColor='black'
-          accentColor='#abacac'
-          theme='minimal'
-        />
-      </div>
-    </div>
+    <Flex className="h-full items-center justify-center gap-x-[20px] w-full ml-[15px] mr-[20px] md:w-[839px]">
+      <button className="p-[5px] active:bg-[#D9D9D9]"  onClick={togglePlayPause}>
+        {isPlaying ? (
+          <img src={"/Pause.svg"} alt="Pause" />
+        ) : (
+          <img src={"/Play.svg"} alt="Play" />
+        )}
+      </button>
+      <span className={styles.currentTime}>{formatTime(currentTime)}</span>
+      <input
+        type="range"
+        value={progress}
+        onChange={handleProgressBarChange}
+        className={styles.progressBar}
+        style={{
+          background: `linear-gradient(to right, #3A3A3A 0%, #3A3A3A ${progress}%, #D9D9D9 ${progress}%, #D9D9D9 100%)`,
+        }}
+      />
+      <MuxAudio
+        ref={audioRef}
+        playbackId={playbackId}
+        placeholder=""
+        autoplay
+        streamType="on-demand"
+        onTimeUpdate={handleTimeUpdate}
+        onLoadedMetadata={handleLoadedMetadata}
+      />
+      <span className={styles.totalTime}>{formatTime(duration)}</span>
+    </Flex>
   )
 }
