a6636e0d5a6299c4af0097e2673d953cd3f0f3e4
Nick Oates <nick@nickoates.com>
2024-02-26 10:48:21 -0800

    revert changes to getUserId

--- apps/site/gql/requests/getUserId.ts
+++ apps/site/gql/requests/getUserId.ts
@@ -1,22 +1,18 @@
 import { type Address } from 'viem'
 import sdk from '../client'
-import { unstable_cache } from 'next/cache'
 
-export const getUserId = unstable_cache(
-  async ({ custodyAddress }: { custodyAddress: Address }) => {
-    if (!custodyAddress) {
-      throw new Error('Address is required')
-    }
+export async function getUserId({
+  custodyAddress,
+}: { custodyAddress: Address }) {
+  if (!custodyAddress) {
+    throw new Error('Address is required')
+  }
 
-    const response = await sdk.UserId({
-      custodyAddress: custodyAddress as string,
-    })
+  const response = await sdk.UserId({
+    custodyAddress: custodyAddress as string,
+  })
 
-    return {
-      userId: response.users?.items?.[0]
-        ? response.users.items[0].userId
-        : null,
-    }
-  },
-  ['userId'],
-)
+  return {
+    userId: response.users?.items?.[0] ? response.users.items[0].userId : null,
+  }
+}
