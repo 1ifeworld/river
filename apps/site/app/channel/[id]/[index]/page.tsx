7a8e9da7525f6f0c0a9573f4c0c1b06b4394810d
Salief <salieflewis@gmail.com>
2024-03-05 09:59:12 -0500

    format

--- apps/site/app/channel/[id]/[index]/page.tsx
+++ apps/site/app/channel/[id]/[index]/page.tsx
@@ -122,7 +122,7 @@ export default async function ItemPage({
   return (
     <Stack className="h-[calc(100dvh-var(--header-height))] md:flex-row">
       <div className="bg-[#F3F4F6] w-full h-full md:w-[78%]">{content}</div>
-      <Separator className="hidden md:block bg-border" orientation='vertical' />
+      <Separator className="hidden md:block bg-border" orientation="vertical" />
       <div className="md:w-[22%]">
         <ItemSidebar
           // @ts-ignore
