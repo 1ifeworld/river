bbe9c9c3d93edc188311fa16607ab75c9a97d7e8
jawndiego <jawndiego@gmail.com>
2024-03-19 21:41:01 -0400

    dev environment initial set up

--- packages/scrypt/constants/addresses.ts
+++ packages/scrypt/constants/addresses.ts
@@ -15,6 +15,9 @@ export const addresses: AddressBook = {
   postGateway: {
     nova: '0x423a602F5e551A25b28eb33eB56B961590aD5290',
   },
+  postGatewayDev: {
+    nova: '0xe805Ff4b95BCB2B6f5c24457F874c6cCCf5f4a93',
+  },
   riverRecovery: {
     optimism: '0xFB0F92f8abdFA25415ADbb6EC0cd9EC33953F29a',
   },
