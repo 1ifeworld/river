6040032a51ecc8fc9dc40cc658826a80ef7527fc
jawndiego <jawndiego@gmail.com>
2024-03-20 21:13:23 -0400

    marquee revert

--- apps/site/components/server/MarqueeWrapper.tsx
+++ apps/site/components/server/MarqueeWrapper.tsx
@@ -5,12 +5,13 @@ export async function MarqueeWrapper() {
   const { users, channels, items } = await getMarqueeData()
 
   console.log('num channels: ', channels)
-
+  
   return (
     <Marquee
-      totalChannels={channels?.items?.[0]?.counter ?? 0}
-      totalItems={items?.items?.[0]?.counter ?? 0}
-      totalUsers={users?.items?.[0]?.counter ?? 0}
+      totalChannels={channels?.items?.[0].counter}
+      totalItems={items?.items?.[0].counter}
+      totalUsers={users?.items?.[0].counter}
     />
   )
 }
+
