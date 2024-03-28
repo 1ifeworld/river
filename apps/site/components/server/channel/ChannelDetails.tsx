a60957947b913c30db79270dabae3ef81ee5570f
0xTranqui <thinkandchill11@gmail.com>
2024-03-28 14:44:07 -0400

    fix channel page item coutns

--- apps/site/components/server/channel/ChannelDetails.tsx
+++ apps/site/components/server/channel/ChannelDetails.tsx
@@ -1,9 +1,12 @@
 import { Stack, Typography } from '@/design-system'
-import type { Channel } from '@/gql'
+import type { Channel, Adds } from '@/gql'
 import { pluralize, unixTimeConverter } from '@/utils'
 
 export async function ChannelDetails({ channel }: { channel: Channel }) {
-  const length = channel.adds?.items ? channel.adds?.items.length : 0
+  
+  const length = channel?.adds?.items
+    ? channel.adds.items.filter((add: Adds) => !add.removed).length
+    : 0;  
 
   return (
     <Stack className="py-8 md:ml-2 gap-y-[3px]">
