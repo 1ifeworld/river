949e44fcaa497cba1d60da25ccff80eafb05e33c
0xTranqui <thinkandchill11@gmail.com>
2024-03-28 19:03:47 -0400

    cleaner code

--- apps/site/components/server/channel/RecentChannels.tsx
+++ apps/site/components/server/channel/RecentChannels.tsx
@@ -1,27 +1,14 @@
-import { Stack, Typography, Public } from '@/design-system'
-import { type ChannelRoles, getMostRecentChannels } from '@/gql'
-import { USER_ID_ZERO } from '@/constants'
 import Link from 'next/link'
+import { Stack, Typography, Public } from '@/design-system'
+import { getMostRecentChannels, type ChannelRoles } from '@/gql'
+import { USER_ID_ZERO } from '@/constants'
+import { sortChannels } from '@/utils'
 
-// biome-ignore lint: Unexpected any. Specify a different type.
-function sort(channels: any[]) {
-  channels.sort((a, b) => {
-    // Check if 'adds' array exists and has at least one item
-    if (a.adds?.items?.length && b.adds?.items?.length) {
-      // Compare timestamps of the first item in the 'adds' array
-      return b.adds.items[0].timestamp - a.adds.items[0].timestamp
-    } else if (a.adds?.items?.length) {
-      // 'b' has no adds, so 'a' should come first
-      return -1
-    } else if (b.adds?.items?.length) {
-      // 'a' has no adds, so 'b' should come first
-      return 1
-    } else {
-      // Neither 'a' nor 'b' has any adds, maintain current order
-      return 0
-    }
-  })
-  return channels
+function checkIsPublic({ roleData }: { roleData: ChannelRoles[] }) {
+  for (let i = 0; i < roleData.length; ++i) {
+    if (roleData[i].rid === USER_ID_ZERO && roleData[i].role > 0) return true
+  }
+  return false
 }
 
 export async function RecentChannels({
@@ -35,14 +22,8 @@ export async function RecentChannels({
     return <Typography>Error fetching channels</Typography>
   }
 
-  const sortedChannels = sort(channels)
-
-  function checkIsPublic({ roleData }: { roleData: ChannelRoles[] }) {
-    for (let i = 0; i < roleData.length; ++i) {
-      if (roleData[i].rid === USER_ID_ZERO && roleData[i].role > 0) return true
-    }
-    return false
-  }
+  // @ts-ignore
+  const sortedChannels = sortChannels(channels)
 
   return (
     <Stack className="hidden md:flex gap-y-[34px]">
