4876123de41f90a3ffa5dedcd229b7007c6fc2d8
0xTranqui <thinkandchill11@gmail.com>
2024-03-28 20:39:54 -0400

    fixes it but still weird"

--- apps/site/components/server/ChannelCard.tsx
+++ apps/site/components/server/ChannelCard.tsx
@@ -66,13 +66,12 @@ export async function ChannelCard({
 
   return (
     <div
-      className={
-        ` w-full
-        ${orientation === 0
-          ? 'flex gap-x-3 items-center justify-start'
-          : 'flex-col gap-y-[10px]'
-        }`
-      }
+      className={` w-full
+        ${
+          orientation === 0
+            ? 'flex gap-x-3 items-center justify-start'
+            : 'flex-col space-y-[10px]'
+        }`}
     >
       <Link href={`/channel/${channel.id}`}>
         {/* <div> */}
@@ -102,9 +101,10 @@ export async function ChannelCard({
             />
           ) : (
             <div
-              className={
-                orientation === 0 ? 'w-full' : `w-full md:w-64`
-              }
+              className={`
+                w-full
+                ${orientation === 1 && 'w-full md:w-64'}
+              `}
             >
               {lastFourNonRemovedItems.length ? (
                 <GenericThumbnailLarge
