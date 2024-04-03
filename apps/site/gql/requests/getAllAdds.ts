2195d82681dddf497a9685c8b3b2537ac3bd1ebe
0xTranqui <thinkandchill11@gmail.com>
2024-04-03 16:05:13 -0400

    logs

--- apps/site/gql/requests/getAllAdds.ts
+++ apps/site/gql/requests/getAllAdds.ts
@@ -4,6 +4,8 @@ import { unstable_cache } from 'next/cache'
 export const getAllAdds = unstable_cache(async () => {
   const response = await sdk.allAdds()
 
+  console.log("response from all sdk.allAdds", response)
+
   return {
     adds: response.addss,
   }
