70e80f5252aff7c6a36cb74e87237e78ffda4d9b
0xTranqui <thinkandchill11@gmail.com>
2024-04-11 10:39:35 -0400

    trying to fix prof page bug

--- apps/site/gql/requests/getChannelsItemsWithUser.ts
+++ apps/site/gql/requests/getChannelsItemsWithUser.ts
@@ -1,13 +1,14 @@
 import sdk from '../client'
 import { unstable_cache } from 'next/cache'
 
-export const getChannelsItemsWithUser = unstable_cache(
+export const getChannelsItemsWithUser = 
+// unstable_cache(
   async ({ userId }: { userId: string }) => {
     const response = await sdk.channelsItemsWithUser({
       userId: BigInt(userId),
     })
 
     return { channels: response.channels, items: response.items }
-  },
-  ['channelsItemsWithUser'],
-)
+  }
+  // ['channelsItemsWithUser'],
+// )
