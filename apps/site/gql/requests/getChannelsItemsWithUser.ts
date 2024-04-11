912892258ecca30075e1a9c2db70877ffa49ef37
0xTranqui <thinkandchill11@gmail.com>
2024-04-11 11:17:47 -0400

    put cache back in

--- apps/site/gql/requests/getChannelsItemsWithUser.ts
+++ apps/site/gql/requests/getChannelsItemsWithUser.ts
@@ -1,14 +1,13 @@
 import sdk from '../client'
 import { unstable_cache } from 'next/cache'
 
-export const getChannelsItemsWithUser = 
-// unstable_cache(
+export const getChannelsItemsWithUser = unstable_cache(
   async ({ userId }: { userId: string }) => {
     const response = await sdk.channelsItemsWithUser({
       userId: userId,
     })
 
     return { channels: response.channels, items: response.items }
-  }
-  // ['channelsItemsWithUser'],
-// )
+  },
+  ['channelsItemsWithUser']
+)
