aad4a67458859001f73e00b072c7d5f4b26e1c0b
jawndiego <jawndiego@gmail.com>
2024-04-12 18:57:30 -0500

    text msg

--- apps/site/components/client/EditUsernameDialog.tsx
+++ apps/site/components/client/EditUsernameDialog.tsx
@@ -249,7 +249,7 @@ export function EditUsernameDialog({ open, setOpen }: UsernameDialogProps) {
                       )}
                       {usernameEditFailed && (
                         <FormMessage className="pt-2 text-red-500">
-                          Can only edit once every two weeks
+                          Username can only be changed once every two weeks
                         </FormMessage>
                       )}
                     </div>
