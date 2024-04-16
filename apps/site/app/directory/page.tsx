8ad94a5efd71e1ab2638118d9c365ab964d90163
jawndiego <jawndiego@gmail.com>
2024-04-15 23:32:24 -0500

    both users and names there

--- apps/site/app/directory/page.tsx
+++ apps/site/app/directory/page.tsx
@@ -2,15 +2,20 @@ import { Stack, Typography, Grid } from '@/design-system'
 import { getMostRecentChannels } from '@/gql'
 import Link from 'next/link'
 import { MarqueeWrapper } from '@/server'
+import { getAllFields } from 'lib/username'
+
+
 
 export default async function Directory() {
   const { channels } = await getMostRecentChannels()
+  const users = await getAllFields({field: 'name'})
 
   if (!channels) {
     return <Typography>No channels added yet</Typography>
   }
 
   channels.sort((a, b) => a.name.localeCompare(b.name))
+  users.sort((x: string, y: string) => x.localeCompare(y)) 
 
   return (
     <section className="flex flex-col justify-center items-center min-h-screen pt-[var(--header-height)]">
@@ -28,6 +33,18 @@ export default async function Directory() {
           </Link>
         ))}
       </Grid>
+      <Grid className="grid-cols-4 gap-4 p-20 justify-center items-center">
+  {users.map((user: string) => (
+    <a
+      href={`/${user}`}
+      key={user}
+      className="flex items-center space-x-[6px]"
+      >
+      <Typography>{user}</Typography>
+    </a>
+  ))}
+</Grid>
     </section>
   )
 }
+
