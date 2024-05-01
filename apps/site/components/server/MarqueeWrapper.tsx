2888570f6527f22363e414150cb8cb30ae89fdd8
0xTranqui <thinkandchill11@gmail.com>
2024-04-30 23:04:02 -0400

    pushing up

--- apps/site/components/server/MarqueeWrapper.tsx
+++ apps/site/components/server/MarqueeWrapper.tsx
@@ -14,8 +14,6 @@ export async function MarqueeWrapper() {
   let marqueeActions: Action[] = []
   const { data } = await getMarqueeData()
 
-  console.log("is getalldata working: ", data)
-
   if (data) {
     //  fetch usernames for adds
     const usernames = await Promise.all(
@@ -26,18 +24,16 @@ export async function MarqueeWrapper() {
       ),
     )
 
-    console.log("is get usernames working ", usernames)
     // fetch metadata for items
     // @ts-ignore
     const { metadata } = await getAddsMetadata(data)
     marqueeActions = data
       .map((add, index) => {
         if (add.removed) return null
-        // const itemMetadata = metadata.data[add.item.uri]
+        const itemMetadata = metadata.data[add.item.uri]
         return {
           userName: usernames[index],
-          // itemName: itemMetadata.name ? itemMetadata.name : '-',
-          itemName: 'null item name',
+          itemName: itemMetadata?.name ?? 'untitled',
           channelName: data[index].channel.name,
           channelId: data[index].channel.id,
           channelIndex: data[index].channelIndex,
