454f4179d3d9b3b226a24a93e9d671fd57354513
Salief <salief@Maxs-Mac-mini.lan>
2024-05-30 16:52:32 -0400

    add get items with user id fn to actions

---
+++ apps/site/lib/actions/getItemsWithUserId.ts
@@ -0,0 +1,15 @@
+'use server'
+
+import { pgPool } from '@/config/pgPool'
+
+export async function getItemsWithUserId(createdById: number) {
+  try {
+    await pgPool.connect()
+    const query =
+      'SELECT COUNT(*) FROM "public"."Item" WHERE "createdById" = $1;'
+    const res = await pgPool.query(query, [createdById])
+    return { itemsWithUserId: res.rows[0].count }
+  } catch (err) {
+    console.error(err)
+  }
+}
